d77dad427ba2766b033e87f9595b73384da8c3f9
Thiebaud Weksteen <tweek@google.com>
2015-11-02 11:44:20 +1100

    Move spd command to a class

--- source/tool/chipsec/utilcmd/spd_cmd.py
+++ source/tool/chipsec/utilcmd/spd_cmd.py
@@ -23,21 +23,14 @@
 
 __version__ = '1.0'
 
-import os
-import sys
 import time
 
-import chipsec_util
-
-
-from chipsec.logger     import *
-from chipsec.file       import *
-
+from chipsec.command     import BaseCommand
 from chipsec.hal.smbus   import *
 from chipsec.hal.spd     import *
 
 
-def spd(argv):
+class SPDCommand(BaseCommand):
     """
     >>> chipsec_util spd detect
     >>> chipsec_util spd dump [device_addr]
@@ -51,69 +44,76 @@ def spd(argv):
     >>> chipsec_util spd read  0xA0 0x0
     >>> chipsec_util spd write 0xA0 0x0 0xAA
     """
-    if 3 > len(argv):
-        print spd.__doc__
-        return
 
-    try:
-        _smbus = SMBus( chipsec_util._cs )
-        _spd   = SPD( _smbus )
-    except BaseException, msg:
-        print msg
-        return
+    def requires_driver(self):
+        # No driver required when printing the util documentation
+        if len(self.argv) < 3:
+            return False
+        return True
 
-    op = argv[2]
-    t = time.time()
-
-    if not _smbus.is_SMBus_supported():
-        logger().log( "[CHIPSEC] SMBus controller is not supported" )
-        return
-    #smbus.display_SMBus_info()
-
-    dev_addr = SPD_SMBUS_ADDRESS
-
-    if( 'detect' == op ):
-
-        logger().log( "[CHIPSEC] Searching for DIMMs with SPD.." )
-        _spd.detect()
-
-    elif( 'dump' == op ):
-
-        if len(argv) > 3:
-            dev = argv[3].upper()
-            dev_addr = chipsec.hal.spd.SPD_DIMM_ADDRESSES[ dev ] if dev in chipsec.hal.spd.SPD_DIMM_ADDRESSES else int(argv[3],16)
-            if not _spd.isSPDPresent( dev_addr ):
-                logger().log( "[CHIPSEC] SPD for DIMM 0x%X is not found" % dev_addr )
-                return
-            _spd.decode( dev_addr )
-        else:
-            _dimms = _spd.detect()
-            for d in _dimms: _spd.decode( d )
- 
-    elif( 'read' == op ) or ( 'write' == op ):
-
-        if len(argv) > 3:
-            dev = argv[3].upper()
-            dev_addr = chipsec.hal.spd.SPD_DIMM_ADDRESSES[ dev ] if dev in chipsec.hal.spd.SPD_DIMM_ADDRESSES else int(argv[3],16)
-        if not _spd.isSPDPresent( dev_addr ):
-            logger().log( "[CHIPSEC] SPD for DIMM 0x%X is not found" % dev_addr )
+    def run(self):
+        if len(self.argv) < 3:
+            print SPDCommand.__doc__
             return
 
-        off = int(argv[4],16)
-        if( 'read' == op ):
-            val      = _spd.read_byte( off, dev_addr )
-            logger().log( "[CHIPSEC] SPD read: offset 0x%X = 0x%X" % (off, val) )
-        elif( 'write' == op ):
-            val      = int(argv[5],16)
-            logger().log( "[CHIPSEC] SPD write: offset 0x%X = 0x%X" % (off, val) )
-            _spd.write_byte( off, val, dev_addr )
+        try:
+            _smbus = SMBus( self.cs )
+            _spd   = SPD( _smbus )
+        except BaseException, msg:
+            print msg
+            return
 
-    else:
-        logger().error( "unknown command-line option '%.32s'" % op )
-        logger().log( spd.__doc__ )
-        return
+        op = self.argv[2]
+        t = time.time()
 
-    logger().log( "[CHIPSEC] (spd) time elapsed %.3f" % (time.time()-t) )
+        if not _smbus.is_SMBus_supported():
+            self.logger.log( "[CHIPSEC] SMBus controller is not supported" )
+            return
+        #smbus.display_SMBus_info()
 
+        dev_addr = SPD_SMBUS_ADDRESS
 
-chipsec_util.commands['spd'] = {'func' : spd, 'start_driver' : True, 'help' : spd.__doc__ }
+        if( 'detect' == op ):
+
+            self.logger.log( "[CHIPSEC] Searching for DIMMs with SPD.." )
+            _spd.detect()
+
+        elif( 'dump' == op ):
+
+            if len(self.argv) > 3:
+                dev = self.argv[3].upper()
+                dev_addr = chipsec.hal.spd.SPD_DIMM_ADDRESSES[ dev ] if dev in chipsec.hal.spd.SPD_DIMM_ADDRESSES else int(self.argv[3],16)
+                if not _spd.isSPDPresent( dev_addr ):
+                    self.logger.log( "[CHIPSEC] SPD for DIMM 0x%X is not found" % dev_addr )
+                    return
+                _spd.decode( dev_addr )
+            else:
+                _dimms = _spd.detect()
+                for d in _dimms: _spd.decode( d )
+     
+        elif( 'read' == op ) or ( 'write' == op ):
+
+            if len(self.argv) > 3:
+                dev = self.argv[3].upper()
+                dev_addr = chipsec.hal.spd.SPD_DIMM_ADDRESSES[ dev ] if dev in chipsec.hal.spd.SPD_DIMM_ADDRESSES else int(self.argv[3],16)
+            if not _spd.isSPDPresent( dev_addr ):
+                self.logger.log( "[CHIPSEC] SPD for DIMM 0x%X is not found" % dev_addr )
+                return
+
+            off = int(self.argv[4],16)
+            if( 'read' == op ):
+                val      = _spd.read_byte( off, dev_addr )
+                self.logger.log( "[CHIPSEC] SPD read: offset 0x%X = 0x%X" % (off, val) )
+            elif( 'write' == op ):
+                val      = int(self.argv[5],16)
+                self.logger.log( "[CHIPSEC] SPD write: offset 0x%X = 0x%X" % (off, val) )
+                _spd.write_byte( off, val, dev_addr )
+
+        else:
+            self.logger.error( "unknown command-line option '%.32s'" % op )
+            self.logger.log( SPDCommand.__doc__ )
+            return
+
+        self.logger.log( "[CHIPSEC] (spd) time elapsed %.3f" % (time.time()-t) )
+
+commands = { 'spd': SPDCommand }
