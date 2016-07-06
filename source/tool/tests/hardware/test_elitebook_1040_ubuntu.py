a3fe15fb4bb9d04b7e0c66a8d05332066ac34624
Thiebaud Weksteen <tweek@google.com>
2016-07-06 11:45:59 +1000

    Add BOOT_MODE to hardware tests

--- source/tool/tests/hardware/test_elitebook_1040_ubuntu.py
+++ source/tool/tests/hardware/test_elitebook_1040_ubuntu.py
@@ -1,23 +1,28 @@
 from tests.hardware import test_ubuntu
 
+
 class EliteBook1040UbuntuTest(test_ubuntu.GenericUbuntuTest):
 
     PRODUCT_NAME = 'HP EliteBook Folio 1040 G1'
     BIOS_VERSION = 'L83 Ver. 01.21'
-    PASS = ["chipsec.modules.common.spi_desc",
-            "chipsec.modules.common.bios_wp",
-            "chipsec.modules.common.spi_lock",
-            "chipsec.modules.common.smrr",
-            "chipsec.modules.common.smm",
-            "chipsec.modules.common.bios_ts",
-            "chipsec.modules.common.bios_smi",
-            "chipsec.modules.smm_dma",
-            "chipsec.modules.remap"]
+    BOOT_MODE = test_ubuntu.GenericUbuntuTest.BOOT_MODE_LEGACY
+    PASS = [
+        "chipsec.modules.common.bios_smi",
+        "chipsec.modules.common.bios_ts",
+        "chipsec.modules.common.bios_wp",
+        "chipsec.modules.common.smm",
+        "chipsec.modules.common.smrr",
+        "chipsec.modules.common.spi_desc",
+        "chipsec.modules.common.spi_lock",
+        "chipsec.modules.remap",
+        "chipsec.modules.smm_dma",
+    ]
 
-    SKIPPED = ["chipsec.modules.common.secureboot.variables",
-               "chipsec.modules.common.uefi.s3bootscript",
-               "chipsec.modules.common.uefi.access_uefispec",
-               "chipsec.modules.module_template"]
+    SKIPPED = [
+        "chipsec.modules.common.secureboot.variables",
+        "chipsec.modules.common.uefi.s3bootscript",
+        "chipsec.modules.common.uefi.access_uefispec",
+    ]
 
     def test_main(self):
         self._generic_main()
