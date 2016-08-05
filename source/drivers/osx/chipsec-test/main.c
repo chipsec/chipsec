c1300a6e7e1330e965c23c2a9b38fc907229d9f4
Thiebaud Weksteen <tweek@google.com>
2016-08-05 11:58:38 +1000

    Clean up test

--- source/drivers/osx/chipsec-test/main.c
+++ source/drivers/osx/chipsec-test/main.c
@@ -29,16 +29,16 @@ typedef int (*test)(int);
 
 int test_pci_ioctl(int dev_chipsec){
     pci_msg_t data;
-    
+
     printf("Testing PCI ioctl...");
-    
+
     data.bus = 0;
     data.device = 0x0;
     data.function = 0x0;
     data.offset = 0x30;
     data.length = 4;
     data.value = 0xdeadbeef;
-    
+
     /* Reading 0:0.0 at offset 0x30, that is BAR0 */
     ioctl(dev_chipsec, CHIPSEC_IOC_RDPCI, &data);
     if(data.value == 0xdeadbeef) {
@@ -49,14 +49,14 @@ int test_pci_ioctl(int dev_chipsec){
     /* Reset BAR0 */
     data.value = 0xFFFFFFFF;
     ioctl(dev_chipsec, CHIPSEC_IOC_WRPCI, &data);
-    
+
     /* Read the value again */
     ioctl(dev_chipsec, CHIPSEC_IOC_RDPCI, &data);
     if(data.value == 0xFFFFFFFF) {
         printf("Error: WRPCI failed\n");
         return -1;
     }
-    
+
     return 0;
 }
 
@@ -64,7 +64,7 @@ int test_mmio_ioctl(int dev_chipsec)
 {
     mmio_msg_t data;
     uint64_t original_value;
-    
+
     printf("Testing MMIO ioctl...");
 
     /* SPIBAR FDATA0 */
@@ -72,13 +72,13 @@ int test_mmio_ioctl(int dev_chipsec)
     data.length = 4;
     ioctl(dev_chipsec, CHIPSEC_IOC_RDMMIO, &data);
     original_value = data.value;
-    
+
     /* Test the 4-bytes write/read */
     data.addr = 0xFED1F810;
     data.length = 4;
     data.value = 0xdeadbeef;
     ioctl(dev_chipsec, CHIPSEC_IOC_WRMMIO, &data);
-    
+
     data.value = 0;
     data.addr = 0xFED1F810;
     data.length = 4;
@@ -87,13 +87,13 @@ int test_mmio_ioctl(int dev_chipsec)
         printf("Error: MMIO 4-bytes Read/Write failed\n");
         return -1;
     }
-    
+
     /* Test the 2-bytes write/read */
     data.addr = 0xFED1F810;
     data.length = 2;
     data.value = 0xcafe;
     ioctl(dev_chipsec, CHIPSEC_IOC_WRMMIO, &data);
-    
+
     data.value = 0;
     data.addr = 0xFED1F810;
     data.length = 2;
@@ -102,13 +102,13 @@ int test_mmio_ioctl(int dev_chipsec)
         printf("Error: MMIO 2-bytes Read/Write failed\n");
         return -1;
     }
-    
+
     /* Test the 1-byte write/read */
     data.addr = 0xFED1F810;
     data.length = 1;
     data.value = 0xee;
     ioctl(dev_chipsec, CHIPSEC_IOC_WRMMIO, &data);
-    
+
     data.value = 0;
     data.addr = 0xFED1F810;
     data.length = 1;
@@ -117,7 +117,7 @@ int test_mmio_ioctl(int dev_chipsec)
         printf("Error: MMIO 1-byte Read/Write failed\n");
         return -1;
     }
-    
+
     /* Restore initial value */
     data.value = original_value;
     data.addr = 0xFED1F810;
@@ -132,9 +132,9 @@ int test_cr_ioctl(int dev_chipsec)
     cr_msg_t data;
     short valid_cr[] = { 0, 2, 3, 4, 8};
     unsigned int len = sizeof(valid_cr)/sizeof(short);
-    
+
     printf("Testing CR ioctl...");
-    
+
     /* Read all the implemented CR register */
     for(i=0; i<len; i++){
         data.register_number = valid_cr[i];
@@ -153,7 +153,7 @@ int main(int argc, const char * argv[])
 
     dev_chipsec = open("/dev/chipsec", O_RDWR);
     num_tests = sizeof(tests)/sizeof(test);
-    
+
     for(i=0; i<num_tests; i++){
         t = tests[i];
         ret = t(dev_chipsec);
