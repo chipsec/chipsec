aa3ef74fcae03b2a64a2195d541588db1d86e2f9
mikhail-gorobets <mikhail.gorobets@gmail.com>
2016-10-04 14:22:34 -0700

    Added General Public License

--- source/tool/chipsec/modules/tools/vmm/virtio.py
+++ source/tool/chipsec/modules/tools/vmm/virtio.py
@@ -1,4 +1,22 @@
-# LICENSE: PUBLIC
+#CHIPSEC: Platform Security Assessment Framework
+#Copyright (c) 2010-2015, Intel Corporation
+# 
+#This program is free software; you can redistribute it and/or
+#modify it under the terms of the GNU General Public License
+#as published by the Free Software Foundation; Version 2.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program; if not, write to the Free Software
+#Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+#Contact information:
+#chipsec@intel.com
+#
 
 """
 QEMU VirtIO info tool
@@ -7,6 +25,7 @@ QEMU VirtIO info tool
    ``chipsec_main.py -i -m tools.vmm.virtio``
    ``chipsec_main.py -i -m tools.vmm.virtio -a 0:6.0``
 """
+
 import re
 from chipsec.module_common import *
 from chipsec.hal.pci       import *
