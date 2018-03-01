57d074042a733c715df497e656d33bb371f08eff
Erik Bjorge <erik.c.bjorge@intel.com>
2018-03-01 14:21:51 -0800

    Disable support for the RWE driver by default.
    Because of known issues using the RWE driver in chipsec it is being
    disabled by default.  It can be enabled manually for experimental
    development.
    
    Signed-off-by: Erik Bjorge <erik.c.bjorge@intel.com>

--- chipsec/helper/helpers.py
+++ chipsec/helper/helpers.py
@@ -26,5 +26,6 @@ from chipsec.helper.efi import *
 from chipsec.helper.linux import *
 from chipsec.helper.osx import *
 from chipsec.helper.win import *
-from chipsec.helper.rwe import *
+# WARNING: Use of RWE driver has known issues. Experimental use only.
+#from chipsec.helper.rwe import *
 
