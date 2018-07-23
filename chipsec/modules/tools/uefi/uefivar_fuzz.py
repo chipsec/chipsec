6e504056fc3f7d7ffe3c13f0efb3f66e1ed317e6
Michaelm396IN <Michael.moorer@intel.com>
2018-07-23 04:09:03 -0700

    update xrange to range for python3 support

--- chipsec/modules/tools/uefi/uefivar_fuzz.py
+++ chipsec/modules/tools/uefi/uefivar_fuzz.py
@@ -96,7 +96,7 @@ class uefivar_fuzz(BaseModule):
 
     def rnd(self, n=1):
         rnum = ''
-        for j in xrange(n):
+        for j in range(n):
             rnum += '%02x' % random.randint(0,255)
         return rnum
         
@@ -177,7 +177,7 @@ class uefivar_fuzz(BaseModule):
                 if FUZZ_NAME:
                     _NAME = ''
                     while not _NAME:
-                        for n in xrange(random.randrange(BOUND_STR)):
+                        for n in range(random.randrange(BOUND_STR)):
                             _NAME += random.choice(string.printable)    
 
                 if FUZZ_GUID  : _GUID   = self.rnd(4)+'-'+self.rnd(2)+'-'+self.rnd(2)+'-'+self.rnd(2)+'-'+self.rnd(6)
