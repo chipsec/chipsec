aaa4d02b5e545cf4fd3fff3828f6eafda59dc15a
Erik Bjorge <erik.c.bjorge@intel.com>
2018-10-24 11:48:05 -0700

    Update build process based on current command file location.
    Signed-off-by: Erik Bjorge <erik.c.bjorge@intel.com>

--- docs/create_manual.cmd
+++ docs/create_manual.cmd
@@ -1,4 +1,5 @@
-rem @echo off
+@echo off
+if "%1" == "" goto usage
 set TARGET=%1
 
 echo "************************ BUILDING DOCUMENTATION ******************************"
@@ -8,18 +9,44 @@ echo "**************************************************************************
 set PYTHONPATH=%TARGET%
 
 pushd %TARGET%
-call python chipsec_main.py -h > %TARGET%\sphinx\options.rst
+call python chipsec_main.py -h > %TARGET%\docs\sphinx\options.rst
 popd
 
-pushd %TARGET%\sphinx
+pushd %TARGET%\docs\sphinx
 
-call sphinx-apidoc -e -f -T -o modules ..
+call sphinx-apidoc -e -f -T -o modules %TARGET%
 call python removeStrRst.py
 
+:: Clean up files that we do not want to process into documenation
+del .\modules\chipsec_tools.*
+del .\modules\tests.*
+del .\modules\setup.rst
+del .\modules\chipsec.rst
+del .\modules\chipsec.cfg.common.rst
+del .\modules\chipsec.chipset.rst
+del .\modules\chipsec.command.rst
+del .\modules\chipsec.defines.rst
+del .\modules\chipsec.file.rst
+del .\modules\chipsec.logger.rst
+del .\modules\chipsec.module.rst
+del .\modules\chipsec.module_common.rst
+del .\modules\chipsec.result_deltas.rst
+del .\modules\chipsec.testcase.rst
+del .\modules\chipsec_main.rst
+del .\modules\chipsec_util.rst
+
 :: create chipsec-manual.pdf
-call sphinx-build -b pdf -D pdf_stylesheets=sstylesheet . ..
+call sphinx-build -b pdf -D pdf_stylesheets=sstylesheet . %TARGET%
+popd
 
 :: remove sphinx folder
 pushd %TARGET%
 rmdir /s /q %TARGET%\.doctrees
 popd
+
+goto end
+
+:usage
+echo Usage: create_manual.cmd [CHIPSEC_PATH]
+
+:end
--- docs/sphinx/removeStrRst.py
+++ docs/sphinx/removeStrRst.py
@@ -1,6 +1,6 @@
 #CHIPSEC: Platform Security Assessment Framework
-#Copyright (c) 2010-2017, Intel Corporation
-# 
+#Copyright (c) 2010-2018, Intel Corporation
+#
 #This program is free software; you can redistribute it and/or
 #modify it under the terms of the GNU General Public License
 #as published by the Free Software Foundation; Version 2.
@@ -21,46 +21,47 @@
 
 import os
 
-thisPath = os.getcwd()
-logsList = os.listdir( thisPath )
-logsPath = thisPath.replace( '\\', '/' )
-           
+doc_path = os.getcwd()
+src_path = os.path.abspath(os.path.join(doc_path, '..\..'))
+log_list = os.listdir( doc_path )
+log_path = doc_path.replace( '\\', '/' )
+
 # Remove subheaders from all rst modules
 def modulesRst():
-    for dirname, subdirs, rstFiles in os.walk( os.path.abspath( '.\\modules' ) ) :
+    for dirname, subdirs, rstFiles in os.walk(os.path.join(doc_path, 'modules')) :
         for file in rstFiles:
             index = 0
             log = ''
-            f = open( 'modules/' + file, 'r' )
+            f = open(os.path.join('modules', file), 'r')
             firstLine = f.readline()
             f.close()
 
-            f = open( 'modules/' + file, 'r' )
+            f = open(os.path.join('modules', file), 'r')
             contents = f.read()
             f.close()
-            
+
             contents = contents.replace( 'Submodules\n----------', '' )
             contents = contents.replace( 'Module contents\n---------------', '' )
             contents = contents.replace( 'Subpackages\n-----------', '' )
             contents = contents.replace( ':undoc-members:', ':private-members:' )
-            
+
             if 'package' in firstLine:
                 index = contents.find( '.. toctree' )
             else:
                 if 'chipsec.modules' in firstLine:
                     modName = file.split( '.' )[ -2 ]
                     modname = '/test_chipsec_main_' + modName + '.log'
-                    if modname[1:] in logsList: 
+                    if modname[1:] in log_list:
                         log = logFile( modname )
-                        logsList.remove( modname[1:] )
-                                
-            f = open( 'modules/' + file, 'w' )
+                        log_list.remove( modname[1:] )
+
+            f = open(os.path.join('modules', file), 'w')
             f.write( contents[index:] + log )
             f.close()
 
 
 def logFile( fileName ):
-    f = open( logsPath + fileName, 'r' )
+    f = open( log_path + fileName, 'r' )
     log = '\n\n.. code-block:: python\n\n'
     while True:
         line = f.readline()
@@ -77,26 +78,26 @@ def logFile( fileName ):
     return log
 
 # Create rst for xml files
-def xmlRst():    
-    for dirname, subdirs, cfgFiles in os.walk( os.path.abspath( '..\\chipsec\\cfg' ) ) :
-        moduleStr = '' 
+def xmlRst():
+    for dirname, subdirs, cfgFiles in os.walk(os.path.join(src_path, 'chipsec', 'cfg')):
+        moduleStr = ''
         for cfg in cfgFiles:
             if ".py" not in cfg:
-                moduleStr += '\tchipsec.cfg.{0}.rst\n'.format(cfg)          
-                f = open( '..\\chipsec\\cfg\\' + cfg, 'r' )
+                moduleStr += '\tchipsec.cfg.{0}.rst\n'.format(cfg)
+                f = open(os.path.join(src_path, 'chipsec', 'cfg', cfg), 'r')
                 xmlContent = f.read()
                 f.close()
                 commentBegins = xmlContent.find('<!--')
                 commentEnds = xmlContent.find('-->')
                 xmlComment = xmlContent[commentBegins+4:commentEnds] + '\n'
-                f = open( '.\\modules\\chipsec.cfg.' + cfg + '.rst', 'w' )
+                f = open(os.path.join(doc_path, 'modules', 'chipsec.cfg.' + cfg + '.rst'), 'w')
                 title = "chipsec.cfg." + cfg
                 f.write( title + "\n" + "="*len(title) + "\n\n" + xmlComment )
                 f.close()
-        f = open( '.\\modules\\chipsec.cfg.rst', 'w' )
-        f.write(".. toctree::\n\n" + moduleStr )
+        f = open(os.path.join(doc_path, 'modules', 'chipsec.cfg.rst'), 'w')
+        f.write(".. toctree::\n\n" + moduleStr)
         f.close()
-    
+
 if __name__ == "__main__":
     modulesRst()
     xmlRst()
