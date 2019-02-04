2cb3e7b8a25145c6d61d19cdb3761583c5682254
Erik Bjorge <erik.c.bjorge@intel.com>
2019-02-04 15:11:10 -0800

    STIBP may not be enabled by default by the OS.
    The OS may choose to only enable STIBP in specific situations to
    reduce the performance impact. An OS may also choosed to only
    enable STIBP for specific processes or by using an opt-in setting.
    It should not be assumed that STIBP will be enabled on all logical
    processors.
    
    Signed-off-by: Erik Bjorge <erik.c.bjorge@intel.com>

--- chipsec/modules/common/cpu/spectre_v2.py
+++ chipsec/modules/common/cpu/spectre_v2.py
@@ -1,5 +1,6 @@
 #CHIPSEC: Platform Security Assessment Framework
 #Copyright (c) 2018, Eclypsium, Inc.
+#Copyright (c) 2019, Intel Corporation
 # 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -48,12 +49,11 @@ WARNING: IBRS/IBPB is supported
          enhanced IBRS is supported
          enhanced IBRS is not enabled by the OS
 WARNING: IBRS/IBPB is supported
-         STIBP is not supported or not enabled by the OS
+         STIBP is not supported
 PASSED : IBRS/IBPB is supported
          enhanced IBRS is supported
          enhanced IBRS is enabled by the OS 
          STIBP is supported
-         STIBP is enabled by the OS
 
 Notes:
 
@@ -146,7 +146,6 @@ class spectre_v2(BaseModule):
 
 
         if arch_cap_supported:
-
             ibrs_enh_supported = True
             #rdcl_mitigation_supported = True
             self.logger.log( "[*] checking enhanced IBRS support in IA32_ARCH_CAPABILITIES..." )
@@ -177,15 +176,13 @@ class spectre_v2(BaseModule):
             else: self.logger.log_bad( "CPU doesn't support enhanced IBRS" ) 
             #if rdcl_mitigation_supported: self.logger.log_good( "CPU supports mitigation for Rogue Data Cache Load (RDCL)" )
             #else: self.logger.log_bad( "CPU doesn't support mitigation for Rogue Data Cache Load (RDCL)" )
-
         else:
             ibrs_enh_supported = False
             self.logger.log_bad( "CPU doesn't support enhanced IBRS" )
 
         ibrs_enabled  = True
-        stibp_enabled = True
+        stibp_enabled_count = 0
         if ibrs_enh_supported:
-
             self.logger.log( "[*] checking if OS is using Enhanced IBRS..." )
             for tid in range(cpu_thread_count):
                 spec_ctrl_msr = 0
@@ -193,7 +190,7 @@ class spectre_v2(BaseModule):
                     spec_ctrl_msr = self.cs.read_register( 'IA32_SPEC_CTRL', tid )
                 except chipsec.helper.oshelper.HWAccessViolationError:
                     self.logger.error( "couldn't read IA32_SPEC_CTRL" )
-                    ibrs_enabled = stibp_enabled = False
+                    ibrs_enabled = False
                     break
 
                 ibrs = self.cs.get_register_field( 'IA32_SPEC_CTRL', spec_ctrl_msr, 'IBRS' )
@@ -204,13 +201,19 @@ class spectre_v2(BaseModule):
                 # ok to access STIBP bit even if STIBP is not supported
                 stibp = self.cs.get_register_field( 'IA32_SPEC_CTRL', spec_ctrl_msr, 'STIBP' )
                 self.logger.log( "[*]   cpu%d: IA32_SPEC_CTRL[STIBP] = %x" % (tid, stibp) )
-                if 0 == stibp:
-                    stibp_enabled = False
+                if 1 == stibp:
+                    stibp_enabled_count += 1
 
-            if ibrs_enabled: self.logger.log_good( "OS enabled Enhanced IBRS (on all logical processors)" )
-            else: self.logger.log_bad( "OS doesn't seem to use Enhanced IBRS" )
-            if stibp_enabled: self.logger.log_good( "OS enabled STIBP (on all logical processors)" )
-            else: self.logger.log_bad( "OS doesn't seem to use STIBP" )
+            if ibrs_enabled:
+                self.logger.log_good( "OS enabled Enhanced IBRS (on all logical processors)" )
+            else:
+                self.logger.log_bad( "OS doesn't seem to use Enhanced IBRS" )
+            if stibp_enabled_count == cpu_thread_count:
+                self.logger.log_good( "OS enabled STIBP (on all logical processors)" )
+            elif stibp_enabled_count > 0:
+                self.logger.log_good( "OS selectively enabling STIBP" )
+            else:
+                self.logger.log_information( "Unable to determine if the OS uses STIBP" )
 
         #
         # Combining results of all checks into final decision
@@ -222,36 +225,28 @@ class spectre_v2(BaseModule):
         #          enhanced IBRS is supported
         #          enhanced IBRS is not enabled by the OS
         # WARNING: IBRS/IBPB is supported
-        #          STIBP is not supported or not enabled by the OS
+        #          STIBP is not supported
         # PASSED : IBRS/IBPB is supported
         #          enhanced IBRS is supported
         #          enhanced IBRS is enabled by the OS 
         #          STIBP is supported
-        #          STIBP is enabled by the OS
         #
         if not ibrs_ibpb_supported:
-
             res = ModuleResult.FAILED
             self.logger.log_failed_check( "CPU mitigation (IBRS) is missing" )
-
         elif not ibrs_enh_supported:
-
             res = ModuleResult.WARNING
             self.logger.log_warn_check( "CPU supports mitigation (IBRS) but doesn't support enhanced IBRS" ) 
-
         elif ibrs_enh_supported and (not ibrs_enabled):
-
             res = ModuleResult.WARNING
             self.logger.log_warn_check( "CPU supports mitigation (enhanced IBRS) but OS is not using it" )
-
         else:
-
-            if (not stibp_supported) or (not stibp_enabled):
+            if (not stibp_supported):
                 res = ModuleResult.WARNING
-                self.logger.log_warn_check( "CPU supports mitigation (enhanced IBRS) but STIBP is not supported/enabled" ) 
+                self.logger.log_warn_check( "CPU supports mitigation (enhanced IBRS) but STIBP is not supported" ) 
             else:
                 res = ModuleResult.PASSED
-                self.logger.log_passed_check( "CPU and OS support hardware mitigations (enhanced IBRS and STIBP)" )
+                self.logger.log_passed_check( "CPU and OS support hardware mitigations" )
 
         self.logger.log_important( "OS may be using software based mitigation (eg. retpoline)" )
 
