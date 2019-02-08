92017036d977201415e4f4bb36f4b2b7bd8442d0
BrentHoltsclaw <brent.holtsclaw@intel.com>
2019-02-07 17:30:14 -0800

    Changes for SPI and SPI Descriptor Util commands

--- chipsec/file.py
+++ chipsec/file.py
@@ -70,7 +70,7 @@ def write_file( filename, buffer, append=False ):
     #  f.write( buffer )
     #f.closed
     perm = 'a' if append else 'w'
-    if type(buffer) == type(bytes()):
+    if isinstance(buffer,bytes) or isinstance(buffer,bytearray):
         perm += 'b'
     try:
         f = open(filename, perm)
