9fe1fbbf8939492ce9fba1b405624ab7e16acd90
BrentHoltsclaw <brent.holtsclaw@intel.com>
2019-04-02 15:43:57 -0700

    Added informational messages to decode_cmd   chipsec_util decode command expects a full spi image.  If the flash descriptor is not found errors will occur   Added informational message which instructs users to use chipsec_util uefi decode for images where the spi descriptor is invalid or does not exist

--- chipsec/utilcmd/decode_cmd.py
+++ chipsec/utilcmd/decode_cmd.py
@@ -81,6 +81,7 @@ class DecodeCommand(BaseCommand):
         (fd_off, fd) = spi_descriptor.get_spi_flash_descriptor( f )
         if (-1 == fd_off) or (fd is None):
             self.logger.error( "Could not find SPI Flash descriptor in the binary '%s'" % rom_file )
+            self.logger.info( "To decode an image without a flash decriptor try chipsec_util uefi decode" )
             return False
 
         self.logger.log( "[CHIPSEC] Found SPI Flash descriptor at offset 0x%x in the binary '%s'" % (fd_off, rom_file) )
@@ -94,6 +95,7 @@ class DecodeCommand(BaseCommand):
         flregs = spi_descriptor.get_spi_regions( fd )
         if flregs is None:
             self.logger.error( "SPI Flash descriptor region is not valid" )
+            self.logger.info( "To decode an image with an invalid flash decriptor try chipsec_util uefi decode" )
             return False
 
         _orig_logname = self.logger.LOG_FILE_NAME
