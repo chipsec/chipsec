fe26a63cdf223800d54e9146ae709d328715337b
BrentHoltsclaw <brent.holtsclaw@intel.com>
2019-06-12 14:11:15 -0700

    Add support to use python build_ext -i command on windows (assuming VS and WDK are installed)
    Signed-off-by: BrentHoltsclaw <brent.holtsclaw@intel.com>

--- chipsec_tools/compression/build.cmd
+++ chipsec_tools/compression/build.cmd
@@ -16,27 +16,30 @@
 @REM #Contact information:
 @REM #chipsec@intel.com
 
+goto :main
+
+:set_buildvars
+@echo Building compression
+@echo off
+@REM # Need to set EDK_TOOLS_PATH and BASE_TOOLS_PATH to current directory
+set EDK_TOOLS_PATH=%CD%
+set BASE_TOOLS_PATH=%CD%
+
+@REM # Now call make and build the compression tools
+call nmake -c
+
+goto :end
+
 @REM # Need to find Visual Studio
+:main
 call get_vsvars.bat
-if not defined VCINSTALLDIR(
+
+if defined VCINSTALLDIR goto :set_buildvars
     @echo.
     @echo !!! ERROR !!!! Cannot find Visual Studio.
     @echo Please download compression tools from https://github.com/tianocore/edk2-BaseTools-win32/archive/master.zip
     @echo Unzip the archive into the chipsec_tools/compression/bin directory
     @echo.
     goto end
-)
-
-@REM # Need to set EDK_TOOLS_PATH and BASE_TOOLS_PATH to current directory and HOST_ARCH
-set EDK_TOOLS_PATH=%CD%
-
-set BASE_TOOLS_PATH=%CD%
-
-if %PROCESSOR_ARCHITECTURE% == AMD64(
-    set HOST_ARCH=X64
-)
-
-@REM # Now call make and build the compression tools
-nmake -c
 
 :end
