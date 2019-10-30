f92572f4b8be89be4b7f06cb4e6b074d995ba188
Evgeny Zinoviev <me@ch1p.io>
2019-10-31 01:04:13 +0300

    Fix decoding of S3 bootscript EDKCompat opcodes
    Signed-off-by: Evgeny Zinoviev <me@ch1p.io>

--- chipsec/hal/uefi_platform.py
+++ chipsec/hal/uefi_platform.py
@@ -1155,6 +1155,10 @@ def decode_s3bs_opcode_edkcompat( data ):
     else:
         op = op_unknown( opcode, size )
         if logger().HAL: logger().warn( 'Unrecognized opcode {:X}'.format(opcode) )
+
+    return op
+
+#
 # @TODO: encode functions are not fully implemented
 #
 def encode_s3bs_opcode_edkcompat( op ):
