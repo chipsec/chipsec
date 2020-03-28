45ef6682347a03d1168717fe206f55fe8f618f1e
brentholtsclaw <brent.holtsclaw@intel.com>
2020-03-27 18:02:33 -0700

    Add ability to use LENGTH and ADMSKXXX regs within client atom
    Depends on #844
    Fix for issue #877
    
    Signed-off-by: brentholtsclaw <brent.holtsclaw@intel.com>

--- chipsec/hal/mmio.py
+++ chipsec/hal/mmio.py
@@ -49,6 +49,15 @@ from chipsec.hal import hal_base
 
 DEFAULT_MMIO_BAR_SIZE = 0x1000
 
+PCI_PCIEXBAR_REG_LENGTH_256MB  = 0x0
+PCI_PCIEXBAR_REG_LENGTH_128MB  = 0x1
+PCI_PCIEXBAR_REG_LENGTH_64MB   = 0x2
+PCI_PCIEXBAR_REG_LENGTH_512MB  = 0x3
+PCI_PCIEXBAR_REG_LENGTH_1024MB = 0x4
+PCI_PCIEXBAR_REG_LENGTH_2048MB = 0x5
+PCI_PCIEXBAR_REG_LENGTH_4096MB = 0x6
+PCI_PCIEBAR_REG_MASK = 0x7FFC000000
+
 class MMIO(hal_base.HALBase):
 
     def __init__(self, cs):
@@ -319,13 +328,22 @@ class MMIO(hal_base.HALBase):
 
     def get_MMCFG_base_address(self):
         (bar_base,bar_size)  = self.get_MMIO_BAR_base_address('MMCFG')
-        # @TODO: temporary w/a
-        #if (Cfg.PCI_PCIEXBAR_REG_LENGTH_256MB == (bar_base & Cfg.PCI_PCIEXBAR_REG_LENGTH_MASK) >> 1):
-        #    bar_base &= ~(Cfg.PCI_PCIEXBAR_REG_ADMSK128|Cfg.PCI_PCIEXBAR_REG_ADMSK64)
-        #elif (Cfg.PCI_PCIEXBAR_REG_LENGTH_128MB == (bar_base & Cfg.PCI_PCIEXBAR_REG_LENGTH_MASK) >> 1):
-        #    bar_base &= ~Cfg.PCI_PCIEXBAR_REG_ADMSK64
-        ##elif (Cfg.PCI_PCIEXBAR_REG_LENGTH_64MB == (bar_base & Cfg.PCI_PCIEXBAR_REG_LENGTH_MASK) >> 1):
-        ##   pass
+        if self.cs.register_has_field("PCI0.0.0_PCIEXBAR", "LENGTH") and not self.cs.is_server():
+            len = self.cs.read_register_field("PCI0.0.0_PCIEXBAR", "LENGTH")
+            if len == PCI_PCIEXBAR_REG_LENGTH_256MB:
+                bar_base &= (PCI_PCIEBAR_REG_MASK << 2)
+            elif len == PCI_PCIEXBAR_REG_LENGTH_128MB:
+                bar_base &= (PCI_PCIEBAR_REG_MASK << 1)
+            if len == PCI_PCIEXBAR_REG_LENGTH_64MB:
+                bar_base &= (PCI_PCIEBAR_REG_MASK << 0)
+            if len == PCI_PCIEXBAR_REG_LENGTH_512MB:
+                bar_base &= (PCI_PCIEBAR_REG_MASK << 3)
+            if len == PCI_PCIEXBAR_REG_LENGTH_1024MB:
+                bar_base &= (PCI_PCIEBAR_REG_MASK << 4)
+            if len == PCI_PCIEXBAR_REG_LENGTH_2048MB:
+                bar_base &= (PCI_PCIEBAR_REG_MASK << 5)
+            if len == PCI_PCIEXBAR_REG_LENGTH_4096MB:
+                bar_base &= (PCI_PCIEBAR_REG_MASK << 6)
         if self.logger.HAL: self.logger.log( '[mmcfg] Memory Mapped CFG Base: 0x{:016X}'.format(bar_base) )
         return bar_base, bar_size
 
