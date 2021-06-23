5a7f5f76918e54f8335db6cf9f787a1bf5f0a2a1
Adrián Herrera Arcila <adr.her.arc.95@gmail.com>
2021-06-23 08:53:59 +0100

    [PATCH] Fix VLA parameter warning (#893)
    Make VLA buffer types consistent in declarations and definitions.
    Resolves build crash when using -Werror due to "vla-parameter" warning.
    
    Signed-off-by: Adrian Herrera <adr.her.arc.95@gmail.com>
    Upstream-status: Backport [https://github.com/google/brotli/commit/0a3944c8c99b8d10cc4325f721b7c273d2b41f7b]

--- chipsec_tools/compression/Brotli/dec/decode.c
+++ chipsec_tools/compression/Brotli/dec/decode.c
@@ -1942,8 +1942,10 @@ static uint32_t BrotliMaxDistanceSymbol(uint32_t ndirect, uint32_t npostfix) {
 }
 
 BrotliDecoderResult BrotliDecoderDecompress(
-    size_t encoded_size, const uint8_t* encoded_buffer, size_t* decoded_size,
-    uint8_t* decoded_buffer) {
+    size_t encoded_size,
+    const uint8_t encoded_buffer[BROTLI_ARRAY_PARAM(encoded_size)],
+    size_t* decoded_size,
+    uint8_t decoded_buffer[BROTLI_ARRAY_PARAM(*decoded_size)]) {
   BrotliDecoderState s;
   BrotliDecoderResult result;
   size_t total_out = 0;
