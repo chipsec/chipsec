bbf334342252306edb72cf957e1a4db489b20a3e
Aaron Frinzell <aaron.frinzell@intel.com>
2021-08-18 17:42:41 -0500

    Add usage documentation to remap.py and memlock.py

--- chipsec/modules/common/memlock.py
+++ chipsec/modules/common/memlock.py
@@ -29,6 +29,14 @@ PASSED : MSR_LT_LOCK_MEMORY[0] is set.
 Hardware registers used:
 MSR_LT_LOCK_MEMORY
 
+Usage:
+  ``chipsec_main -m common.memlock``
+
+Example:
+    >>> chipsec_main.py -m common.memlock
+
+.. note:: This module will not run on Atom based platforms.
+
 """
 
 from chipsec.module_common import BaseModule, ModuleResult
@@ -52,12 +60,16 @@ class memlock(BaseModule):
         # Workaround for Atom based processors.  Accessing this MSR on these systems
         # causes a GP fault and can't be caught in UEFI Shell.
         if not self.cs.is_atom():
-            if self.cs.is_register_defined( 'MSR_LT_LOCK_MEMORY' ):
-                return self.cs.register_has_field( 'MSR_LT_LOCK_MEMORY', 'LT_LOCK' )
+            if self.cs.register_has_field('MSR_LT_LOCK_MEMORY', 'LT_LOCK'):
+                return True
+            else:
+                self.logger.log("'MSR_LT_LOCK_MEMORY.LT_LOCK' not defined for platform.  Skipping module.")
+        else:
+            self.logger.log('Found an Atom based platform.  Skipping module.')
         return False
 
     def check_MSR_LT_LOCK_MEMORY( self ):
-        self.logger.log( "[X] Checking MSR_LT_LOCK_MEMORY status" )
+        self.logger.log("[X] Checking MSR_LT_LOCK_MEMORY status")
         status = False
         for tid in range(self.cs.msr.get_cpu_thread_count()):
             lt_lock_msr = 0
@@ -76,14 +88,14 @@ class memlock(BaseModule):
 
     def run( self, module_argv ):
         self.res = ModuleResult.PASSED
-        self.logger.start_test( "Check MSR_LT_LOCK_MEMORY" )
+        self.logger.start_test("Check MSR_LT_LOCK_MEMORY")
         check_MSR_LT_LOCK_MEMORY_test_fail = self.check_MSR_LT_LOCK_MEMORY()
 
         if check_MSR_LT_LOCK_MEMORY_test_fail == True:
-            self.logger.log_failed_check( "Check failed. MSR_LT_LOCK_MEMORY isn't configured correctly" )
+            self.logger.log_failed("Check failed. MSR_LT_LOCK_MEMORY isn't configured correctly")
             self.res = ModuleResult.FAILED
         else:
-            self.logger.log_passed_check('Check have successfully passed')
+            self.logger.log_passed('Check have successfully passed')
             self.res = ModuleResult.PASSED
 
         return self.res
