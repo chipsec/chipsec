7f70356c4a86722b9f7a7d1e3b46dd8d8468da7c
Aaron Frinzell <aaron.frinzell@intel.com>
2021-09-15 11:43:11 -0500

    Updates to spd_wd.py

--- chipsec/modules/common/spd_wd.py
+++ chipsec/modules/common/spd_wd.py
@@ -1,6 +1,6 @@
 # CHIPSEC: Platform Security Assessment Framework
 # Copyright (c) 2019, Eclypsium, Inc.
-# Copyright (c) 2019-2020, Intel Corporation
+# Copyright (c) 2019-2021, Intel Corporation
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -19,9 +19,8 @@
 This module checks that SPD Write Disable bit in SMBus controller has been set
 
 References:
-
-Intel 8 Series/C220 Series Chipset Family Platform Controller Hub datasheet
-Intel 300 Series Chipset Families Platform Controller Hub datasheet
+    Intel 8 Series/C220 Series Chipset Family Platform Controller Hub datasheet
+    Intel 300 Series Chipset Families Platform Controller Hub datasheet
 
 This module checks the following:
 
@@ -38,6 +37,17 @@ The module returns the following results:
 Hardware registers used:
 
     SMBUS_HCFG
+
+Usage:
+    ``chipsec_main -m common.spd_wd``
+
+Examples:
+    >>> chipsec_main.py -m common.spd_wd
+
+.. NOTE::
+    This module will only run if:
+        - SMBUS device is enabled
+        - SMBUS_HCFG.SPD_WD is defined for the platform
 """
 
 from chipsec.module_common import BaseModule, ModuleResult
@@ -51,36 +61,45 @@ class spd_wd(BaseModule):
         self.cs.set_scope("8086.SMBUS")
 
     def is_supported(self):
-        return self.cs.is_device_enabled( 'SMBUS' ) and self.cs.register_has_field('SMBUS_HCFG', 'SPD_WD')
+        if self.cs.is_device_enabled('SMBUS'):
+            if self.cs.register_has_field('SMBUS_HCFG', 'SPD_WD'):
+                return True
+            else:
+                self.logger.log_important('SMBUS_HCFG.SPD_WD is not defined for this platform.  Skipping module.')
+        else:
+            self.logger.log_important('SMBUS device appears disabled.  Skipping module.')
+        self.res = ModuleResult.NOTAPPLICABLE
+        return False
 
     def check_spd_wd(self):
-        self.logger.start_test( "SPD Write Disable" )
-
         try:
-            _smbus = SMBus( self.cs )
-            _spd   = SPD( _smbus )
+            _smbus = SMBus(self.cs)
+            _spd   = SPD(_smbus)
         except BaseException as msg:
-            self.logger.error( msg )
+            self.logger.log_error(msg)
             self.res = ModuleResult.ERROR
             return self.res
 
-        spd_wd_reg = self.cs.read_register( 'SMBUS_HCFG' )
-        spd_wd = self.cs.get_register_field( 'SMBUS_HCFG', spd_wd_reg, 'SPD_WD' )
+        spd_wd_reg = self.cs.read_register('SMBUS_HCFG')
+        spd_wd = self.cs.get_register_field('SMBUS_HCFG', spd_wd_reg, 'SPD_WD')
 
-        dimms = _spd.detect()
+        self.cs.print_register('SMBUS_HCFG',spd_wd_reg)
 
         if 1 == spd_wd:
-            self.logger.log_passed_check( "SPD Write Disable is set" )
+            self.logger.log_passed("SPD Write Disable is set")
             self.res = ModuleResult.PASSED
         else:
-            if dimms:
-                self.logger.log_failed_check( "SPD Write Disable is not set and SPDs were detected" )
+            if _spd.detect():
+                self.logger.log_failed("SPD Write Disable is not set and SPDs were detected")
                 self.res = ModuleResult.FAILED
             else:
-                self.logger.log_information_check( "SPD Write Disable is not set, but no SPDs detected" )
+                self.logger.log_information("SPD Write Disable is not set, but no SPDs detected")
                 self.res = ModuleResult.INFORMATION
 
         return self.res
 
     def run( self, module_argv ):
+        self.logger.start_test("SPD Write Disable")
+        self.logger.log('')
+
         return self.check_spd_wd()
