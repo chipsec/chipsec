e1e5632c22da68e6227725a1455f779a192c020c
Frinzell, Aaron <aaron.frinzell@intel.com>
2022-04-06 14:51:55 -0500

    Add usage documentation to bios_smi.py
    Signed-off-by: Frinzell, Aaron <aaron.frinzell@intel.com>

--- chipsec/modules/common/bios_smi.py
+++ chipsec/modules/common/bios_smi.py
@@ -1,21 +1,21 @@
-#CHIPSEC: Platform Security Assessment Framework
-#Copyright (c) 2010-2021, Intel Corporation
+# CHIPSEC: Platform Security Assessment Framework
+# Copyright (c) 2010-2021, Intel Corporation
 #
-#This program is free software; you can redistribute it and/or
-#modify it under the terms of the GNU General Public License
-#as published by the Free Software Foundation; Version 2.
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; Version 2.
 #
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-#You should have received a copy of the GNU General Public License
-#along with this program; if not, write to the Free Software
-#Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
-#Contact information:
-#chipsec@intel.com
+# Contact information:
+# chipsec@intel.com
 #
 
 
@@ -25,10 +25,21 @@ The module checks that SMI events configuration is locked down
 - TCO SMI Enable/TCO Lock
 
 References:
+    - `Setup for Failure: Defeating SecureBoot <http://syscan.org/index.php/download/get/6e597f6067493dd581eed737146f3afb/SyScan2014_CoreyKallenberg_SetupforFailureDefeatingSecureBoot.zip>`_ by Corey Kallenberg, Xeno Kovah, John Butterworth, Sam Cornwell
+    - `Summary of Attacks Against BIOS and Secure Boot <https://www.defcon.org/images/defcon-22/dc-22-presentations/Bulygin-Bazhaniul-Furtak-Loucaides/DEFCON-22-Bulygin-Bazhaniul-Furtak-Loucaides-Summary-of-attacks-against-BIOS-UPDATED.pdf>`_
 
-`Setup for Failure: Defeating SecureBoot <http://syscan.org/index.php/download/get/6e597f6067493dd581eed737146f3afb/SyScan2014_CoreyKallenberg_SetupforFailureDefeatingSecureBoot.zip>`_ by Corey Kallenberg, Xeno Kovah, John Butterworth, Sam Cornwell
+Usage:
+    ``chipsec_main -m common.bios_smi``
 
-`Summary of Attacks Against BIOS and Secure Boot` (https://www.defcon.org/images/defcon-22/dc-22-presentations/Bulygin-Bazhaniul-Furtak-Loucaides/DEFCON-22-Bulygin-Bazhaniul-Furtak-Loucaides-Summary-of-attacks-against-BIOS-UPDATED.pdf)
+Examples:
+    >>> chipsec_main.py -m common.bios_smi
+
+Registers used:
+    - SmmBiosWriteProtection (Control)
+    - TCOSMILock (Control)
+    - SMILock (Control)
+    - BiosWriteEnable (Control)
+
 """
 
 from chipsec.module_common import BaseModule, ModuleResult, MTAG_BIOS, MTAG_SMM
@@ -42,24 +53,24 @@ class bios_smi(BaseModule):
         BaseModule.__init__(self)
 
     def is_supported(self):
-        if not self.cs.is_control_defined( 'SmmBiosWriteProtection' ) or \
-           not self.cs.is_control_defined( 'TCOSMILock' ) or \
-           not self.cs.is_control_defined( 'SMILock' ) or \
-           not self.cs.is_control_defined( 'BiosWriteEnable'):
+        if not self.cs.is_control_defined('SmmBiosWriteProtection') or \
+           not self.cs.is_control_defined('TCOSMILock') or \
+           not self.cs.is_control_defined('SMILock') or \
+           not self.cs.is_control_defined('BiosWriteEnable'):
             self.res = ModuleResult.NOTAPPLICABLE
             return False
         return True
 
     def check_SMI_locks(self):
 
-        self.logger.start_test( "SMI Events Configuration" )
-
         #
         # Checking SMM_BWP first in BIOS control to warn if SMM write-protection of the BIOS is not enabled
         #
-        smm_bwp = self.cs.get_control( 'SmmBiosWriteProtection' )
-        if 0 == smm_bwp: self.logger.log_bad( "SMM BIOS region write protection has not been enabled (SMM_BWP is not used)\n" )
-        else:            self.logger.log_good( "SMM BIOS region write protection is enabled (SMM_BWP is used)\n" )
+        smm_bwp = self.cs.get_control('SmmBiosWriteProtection')
+        if 0 == smm_bwp:
+            self.logger.log_bad("SMM BIOS region write protection has not been enabled (SMM_BWP is not used)\n")
+        else:
+            self.logger.log_good("SMM BIOS region write protection is enabled (SMM_BWP is used)\n")
 
         ok = True
         warn = False
@@ -67,66 +78,61 @@ class bios_smi(BaseModule):
         #
         # Checking if global SMI and TCO SMI are enabled (GBL_SMI_EN and TCO_EN bits in SMI_EN register)
         #
-        if self.cs.is_control_defined( 'TCOSMIEnable' ) and self.cs.is_control_defined( 'GlobalSMIEnable' ):
-            self.logger.log( "[*] Checking SMI enables.." )
-            tco_en     = self.cs.get_control( 'TCOSMIEnable' )
-            gbl_smi_en = self.cs.get_control( 'GlobalSMIEnable' )
+        if self.cs.is_control_defined('TCOSMIEnable') and self.cs.is_control_defined('GlobalSMIEnable'):
+            self.logger.log("[*] Checking SMI enables..")
+            tco_en     = self.cs.get_control('TCOSMIEnable')
+            gbl_smi_en = self.cs.get_control('GlobalSMIEnable')
             self.logger.log( "    Global SMI enable: {:d}".format(gbl_smi_en) )
             self.logger.log( "    TCO SMI enable   : {:d}".format(tco_en) )
 
             if gbl_smi_en != 1:
                 ok = False
-                self.logger.log_bad( "Global SMI is not enabled" )
-            elif tco_en != 1 and smm_bwp != 1:
+                self.logger.log_bad("Global SMI is not enabled")
+            elif (tco_en != 1) and (smm_bwp != 1):
                 warn = True
-                self.logger.warn( "TCO SMI is not enabled. BIOS may not be using it" )
-            elif tco_en != 1 and smm_bwp == 1:
+                self.logger.log_warning("TCO SMI is not enabled. BIOS may not be using it")
+            elif (tco_en != 1) and (smm_bwp == 1):
                 ok = False
                 self.logger.log_bad("TCO SMI should be enabled if using SMM BIOS region protection")
             else:
-                self.logger.log_good( "All required SMI events are enabled" )
+                self.logger.log_good("All required SMI events are enabled")
             self.logger.log('')
-
-            self.logger.log( "[*] Checking SMI configuration locks.." )
+            self.logger.log("[*] Checking SMI configuration locks..")
 
         #
         # Checking TCO_LOCK
         #
-        tco_lock = self.cs.get_control( 'TCOSMILock')
+        tco_lock = self.cs.get_control('TCOSMILock')
         if tco_lock != 1:
             ok = False
-            self.logger.log_bad( "TCO SMI event configuration is not locked. TCO SMI events can be disabled" )
+            self.logger.log_bad("TCO SMI event configuration is not locked. TCO SMI events can be disabled")
         else:
-            self.logger.log_good( "TCO SMI configuration is locked (TCO SMI Lock)" )
+            self.logger.log_good("TCO SMI configuration is locked (TCO SMI Lock)")
 
         #
         # Checking SMI_LOCK
         #
-        smi_lock = self.cs.get_control( 'SMILock' )
+        smi_lock = self.cs.get_control('SMILock')
         if smi_lock != 1:
             ok = False
-            self.logger.log_bad( "SMI events global configuration is not locked. SMI events can be disabled" )
+            self.logger.log_bad("SMI events global configuration is not locked. SMI events can be disabled")
         else:
-            self.logger.log_good( "SMI events global configuration is locked (SMI Lock)" )
+            self.logger.log_good("SMI events global configuration is locked (SMI Lock)")
         self.logger.log('')
 
         if ok and not warn:
             res = ModuleResult.PASSED
-            self.logger.log_passed( "All required SMI sources seem to be enabled and locked" )
+            self.logger.log_passed("All required SMI sources seem to be enabled and locked")
         elif ok and warn:
             res = ModuleResult.WARNING
             self.logger.log_warning("One or more warnings detected when checking SMI enable state")
         else:
             res = ModuleResult.FAILED
-            self.logger.log_failed( "Not all required SMI sources are enabled and locked" )
+            self.logger.log_failed("Not all required SMI sources are enabled and locked")
         return res
 
 
-
-    # --------------------------------------------------------------------------
-    # run( module_argv )
-    # Required function: run here all tests from this module
-    # --------------------------------------------------------------------------
-    def run( self, module_argv ):
+    def run(self, module_argv):
+        self.logger.start_test("SMI Events Configuration")
         self.res = self.check_SMI_locks()
         return self.res
