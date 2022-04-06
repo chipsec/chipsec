a70bc0bd9c940b7162f9541a7a34c4114ec8a8a8
Frinzell, Aaron <aaron.frinzell@intel.com>
2022-04-06 15:27:51 -0500

    Add usage documentation to spi_desc.py
    Signed-off-by: Frinzell, Aaron <aaron.frinzell@intel.com>

--- chipsec/modules/common/spi_desc.py
+++ chipsec/modules/common/spi_desc.py
@@ -1,29 +1,41 @@
-#CHIPSEC: Platform Security Assessment Framework
-#Copyright (c) 2010-2021, Intel Corporation
+# CHIPSEC: Platform Security Assessment Framework
+# Copyright (c) 2010-2021, Intel Corporation
 #
-#This program is free software; you can redistribute it and/or
-#modify it under the terms of the GNU General Public License
-#as published by the Free Software Foundation; Version 2.
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; Version 2.
 #
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-#You should have received a copy of the GNU General Public License
-#along with this program; if not, write to the Free Software
-#Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
-#Contact information:
-#chipsec@intel.com
+# Contact information:
+# chipsec@intel.com
 #
-
 
 
 """
-The SPI Flash Descriptor indicates read/write permissions for devices to access regions of the flash memory. This module simply reads the Flash Descriptor and checks that software cannot modify the Flash Descriptor itself. If software can write to the Flash Descriptor, then software could bypass any protection defined by it. While often used for debugging, this should not be the case on production systems.
+The SPI Flash Descriptor indicates read/write permissions for devices to access regions of the flash memory. 
+This module simply reads the Flash Descriptor and checks that software cannot modify the Flash Descriptor itself. 
+If software can write to the Flash Descriptor, then software could bypass any protection defined by it. 
+While often used for debugging, this should not be the case on production systems.
 
 This module checks that software cannot write to the flash descriptor.
+
+Usage:
+    ``chipsec_main -m common.spi_desc``
+
+Examples:
+    >>> chipsec_main.py -m common.spi_desc
+
+Registers used:
+    - FRAP.BRRA
+    - FRAP.BRWA
 
 """
 
@@ -43,13 +55,12 @@ class spi_desc(BaseModule):
     ##
     # Displays the SPI Regions Access Permissions
     def check_flash_access_permissions(self):
-        self.logger.start_test( "SPI Flash Region Access Control" )
 
         res = ModuleResult.PASSED
-        frap = self.cs.read_register( 'FRAP' )
-        self.cs.print_register( 'FRAP', frap )
-        brra = self.cs.get_register_field( 'FRAP', frap, 'BRRA' )
-        brwa = self.cs.get_register_field( 'FRAP', frap, 'BRWA' )
+        frap = self.cs.read_register('FRAP')
+        self.cs.print_register('FRAP', frap)
+        brra = self.cs.get_register_field('FRAP', frap, 'BRRA')
+        brwa = self.cs.get_register_field('FRAP', frap, 'BRWA')
 
         self.logger.log("[*] Software access to SPI flash regions: read = 0x{:02X}, write = 0x{:02X}".format(brra, brwa) )
         if brwa & (1 << FLASH_DESCRIPTOR):
@@ -68,6 +79,7 @@ class spi_desc(BaseModule):
     # run( module_argv )
     # Required function: run here all tests from this module
     # --------------------------------------------------------------------------
-    def run( self, module_argv ):
+    def run(self, module_argv):
+        self.logger.start_test("SPI Flash Region Access Control")
         self.res = self.check_flash_access_permissions()
         return self.res
