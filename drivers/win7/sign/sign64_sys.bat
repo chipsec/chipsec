ab2e8095f70a2c4b6b664064ee843d69c264d6db
startergo <7897244+startergo@users.noreply.github.com>
2022-04-16 12:10:09 -0400

    Update sign64_sys.bat
    Update sign server.

--- drivers/win7/sign/sign64_sys.bat
+++ drivers/win7/sign/sign64_sys.bat
@@ -1,2 +1,2 @@
 copy /Y ..\..\..\chipsec\helper\win\win7_amd64\chipsec_hlpr.sys 
-SignTool sign /s ChipsecCertStore  /t http://timestamp.verisign.com/scripts/timestamp.dll   chipsec_hlpr.sys
+SignTool sign /fdws /t http://timestamp.digicert.com   chipsec_hlpr.sys
