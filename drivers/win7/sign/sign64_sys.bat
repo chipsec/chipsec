3ae35ec43a65500ef46ebb67c1ee369fe8b51540
startergo <7897244+startergo@users.noreply.github.com>
2022-04-16 11:47:57 -0400

    Update sign64_sys.bat
    Update path

--- drivers/win7/sign/sign64_sys.bat
+++ drivers/win7/sign/sign64_sys.bat
@@ -1,3 +1,2 @@
-copy /Y ..\sys\amd64\chipsec_hlpr.sys 
+copy /Y ..\..\..\chipsec\helper\win\win7_amd64\chipsec_hlpr.sys 
 SignTool sign /s ChipsecCertStore  /t http://timestamp.verisign.com/scripts/timestamp.dll   chipsec_hlpr.sys
-
