67f0aba857ad4b412d74796d5c7b9056c5523bc7
Frinzell, Aaron <aaron.frinzell@intel.com>
2022-04-25 16:11:36 -0500

    Update is_suported() in bios_ts.py
    Signed-off-by: Frinzell, Aaron <aaron.frinzell@intel.com>

--- chipsec/modules/common/bios_ts.py
+++ chipsec/modules/common/bios_ts.py
@@ -1,23 +1,22 @@
-#CHIPSEC: Platform Security Assessment Framework
-#Copyright (c) 2010-2020, Intel Corporation
+# CHIPSEC: Platform Security Assessment Framework
+# Copyright (c) 2010-2020, Intel Corporation
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; Version 2.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# 
+# Contact information:
+# chipsec@intel.com
 #
-#This program is free software; you can redistribute it and/or
-#modify it under the terms of the GNU General Public License
-#as published by the Free Software Foundation; Version 2.
-#
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
-#
-#You should have received a copy of the GNU General Public License
-#along with this program; if not, write to the Free Software
-#Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-#
-#Contact information:
-#chipsec@intel.com
-#
-
 
 
 """
@@ -47,17 +46,15 @@ class bios_ts(BaseModule):
         BaseModule.__init__(self)
 
     def is_supported(self):
-        return True
+        if self.cs.is_control_defined('BiosInterfaceLockDown'):
+            return True
+        self.logger.log_important('BiosInterfaceLockDown control not defined for platform.  Skipping module.')
+        self.res = ModuleResult.NOTAPPLICABLE
+        return False
 
     def check_bios_iface_lock(self):
-
-        bild = 0
-        if self.cs.is_control_defined('BiosInterfaceLockDown'):
-            bild = self.cs.get_control('BiosInterfaceLockDown')
-            self.logger.log( "[*] BiosInterfaceLockDown (BILD) control = {:d}".format(bild) )
-        else:
-            self.logger.log_error("BiosInterfaceLockDown (BILD) control is not defined")
-            return ModuleResult.ERROR
+        bild = self.cs.get_control('BiosInterfaceLockDown')
+        self.logger.log("[*] BiosInterfaceLockDown (BILD) control = {:d}".format(bild))
 
         if self.cs.is_control_defined('TopSwapStatus'):
             if self.cs.is_control_all_ffs('TopSwapStatus'):
@@ -65,7 +62,7 @@ class bios_ts(BaseModule):
                 self.logger.verbose_log('TopSwapStatus read returned all 0xFs.')
             else:
                 tss = self.cs.get_control('TopSwapStatus')
-                self.logger.log( "[*] BIOS Top Swap mode is {} (TSS = {:d})".format('enabled' if (1==tss) else 'disabled', tss) )
+                self.logger.log("[*] BIOS Top Swap mode is {} (TSS = {:d})".format('enabled' if (1==tss) else 'disabled', tss))
 
         if self.cs.is_control_defined('TopSwap'):
             if self.cs.is_control_all_ffs('TopSwap'):
@@ -73,9 +70,9 @@ class bios_ts(BaseModule):
                 self.logger.verbose_log('TopSwap read returned all 0xFs.')
             else:
                 ts = self.cs.get_control('TopSwap')
-                self.logger.log( "[*] RTC TopSwap control (TS) = {:x}".format(ts) )
+                self.logger.log("[*] RTC TopSwap control (TS) = {:x}".format(ts))
 
-        if 0 == bild:
+        if bild == 0:
             res = ModuleResult.FAILED
             self.logger.log_failed("BIOS Interface is not locked (including Top Swap Mode)")
         else:
@@ -83,11 +80,7 @@ class bios_ts(BaseModule):
             self.logger.log_passed("BIOS Interface is locked (including Top Swap Mode)")
         return res
 
-    # --------------------------------------------------------------------------
-    # run( module_argv )
-    # Required function: run here all tests from this module
-    # --------------------------------------------------------------------------
-    def run(self, module_argv ):
+    def run(self, module_argv):
         self.logger.start_test("BIOS Interface Lock (including Top Swap Mode)")
         self.res = self.check_bios_iface_lock()
         return self.res
