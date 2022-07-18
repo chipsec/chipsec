344043b37dab03a16fff6d14233ff7aa36ccf570
Sara Batllori <sara.batllori@intel.com>
2022-07-18 10:04:28 -0700

    Remove sphinx warnings
    Signed-off-by: Sara Batllori <sara.batllori@intel.com>

--- chipsec/modules/tools/smm/smm_ptr.py
+++ chipsec/modules/tools/smm/smm_ptr.py
@@ -23,8 +23,8 @@
 A tool to test SMI handlers for pointer validation vulnerabilities
 
 Reference:
-    - CanSecWest 2015: `A New Class of Vulnerability in SMI Handlers of BIOS/UEFI Firmware <https://cansecwest.com/slides/2015/A%20New%20Class%20of%20Vulnin%20SMI%20-%20Andrew%20Furtak.pdf>`_ (dead?)
-    - c7zero.info: `A New Class of Vulnerability in SMI Handlers of BIOS/UEFI Firmware <http://www.c7zero.info/stuff/ANewClassOfVulnInSMIHandlers_csw2015.pdf>`_
+    - Presented in CanSecWest 2015:
+        - c7zero.info: `A New Class of Vulnerability in SMI Handlers of BIOS/UEFI Firmware <http://www.c7zero.info/stuff/ANewClassOfVulnInSMIHandlers_csw2015.pdf>`_
 
 
 Usage:
