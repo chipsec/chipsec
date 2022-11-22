add42a0d7f52b94d0f23a0ecac458c62fe0a2e63
brentholtsclaw <brent.holtsclaw@intel.com>
2022-11-22 09:55:52 -0800

    Decouple chipsec_main from logger
    Remove start_test from logger
    Enhance TestCase class to add start_test funtionality
    Change logic within run_loaded modules to use TestCase opposed to logger
    Add fstring to testcase.py
    Add Type information to testcase.py
    
    Signed-off-by: brentholtsclaw <brent.holtsclaw@intel.com>

--- chipsec/testcase.py
+++ chipsec/testcase.py
@@ -1,5 +1,5 @@
 # CHIPSEC: Platform Security Assessment Framework
-# Copyright (c) 2018-2020, Intel Corporation
+# Copyright (c) 2018-2022, Intel Corporation
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -24,6 +24,8 @@ import os
 from collections import OrderedDict
 import xml.etree.ElementTree as ET
 import xml.dom.minidom
+from chipsec.logger import logger
+from typing import Dict, Type, Optional
 
 
 class ExitCode:
@@ -55,7 +57,35 @@ class ExitCode:
 """
 
 
-class ChipsecResults:
+class TestCase():
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.result = ''
+        self.output = ''
+        self.argv = ''
+        self.desc = ''
+        self.startTime = None
+        self.endTime = None
+        self.time = None
+
+    def get_fields(self) -> Dict[str, str]:
+        return {'name': self.name, 'output': self.output, 'result': self.result}
+
+    def start_module(self) -> None:
+        """Displays a banner for the module name provided."""
+        text = f'\n[*] running module: {self.name}'
+        logger().log_heading(text)
+        self.startTime = time.time()
+        self.desc = self.name
+
+    def end_module(self, result: str, arg: str) -> None:
+        self.result = result
+        self.argv = arg
+        self.endTime = time.time()
+        self.time = self.endTime - self.startTime
+
+
+class ChipsecResults():
     def __init__(self):
         self.test_cases = []
         self.properties = None
@@ -63,16 +93,13 @@ class ChipsecResults:
         self.exceptions = []
         self.time = None
 
-    def add_properties(self, properties):
+    def add_properties(self, properties: Dict[str, str]) -> None:
         self.properties = properties
 
-    def add_testcase(self, test):
+    def add_testcase(self, test: Type[TestCase]) -> None:
         self.test_cases.append(test)
 
-    def flush_testcases(self) -> None:
-        self.__init__()
-
-    def get_current(self):
+    def get_current(self) -> Type[TestCase]:
         if len(self.test_cases) == 0 or self.summary:
             return None
         return self.test_cases[len(self.test_cases) - 1]
@@ -80,9 +107,7 @@ class ChipsecResults:
     def add_exception(self, name):
         self.exceptions.append(str(name))
 
-    def order_summary(self):
-        if self.time is None:
-            self.set_time()
+    def order_summary(self) -> None:
         self.summary = True
         ret = OrderedDict()
         passed = []
@@ -121,7 +146,7 @@ class ChipsecResults:
         ret['exceptions'] = self.exceptions
         return ret
 
-    def get_return_code(self):
+    def get_return_code(self) -> int:
         summary = self.order_summary()
         if len(summary['failed to run']) != 0:
             return ExitCode.ERROR
@@ -140,7 +165,7 @@ class ChipsecResults:
         else:
             return ExitCode.OK
 
-    def set_time(self, pTime=None):
+    def set_time(self, pTime: Optional[float] = None) -> None:
         """Sets the time"""
         if pTime is not None:
             self.time = pTime
@@ -150,17 +175,17 @@ class ChipsecResults:
             else:
                 self.time = self.test_cases[0].time
 
-    def get_results(self):
+    def get_results(self) -> Dict[str, int]:
         results = {}
         for test in self.test_cases:
-            results[test.name] = {"result": test.result}
+            results[test.name] = {'result': test.result}
         return results
 
-    def xml_summary(self):
+    def xml_summary(self) -> str:
         summary = self.order_summary()
         xml_element = ET.Element("Summary")
         for k in summary.keys():
-            temp = dict()
+            temp = {}
             if k == 'total':
                 temp['name'] = k
                 temp['total'] = "{:d}".format(summary[k])
@@ -174,44 +199,45 @@ class ChipsecResults:
                     n_element.text = mod
         return ET.tostring(xml_element, None, None)
 
-    def json_summary(self):
+    def json_summary(self) -> str:
         summary = self.order_summary()
         js = json.dumps(summary, sort_keys=False, indent=2, separators=(',', ': '))
         return js
 
-    def json_full(self):
+    def json_full(self) -> str:
         summary = self.get_results()
         js = json.dumps(summary, sort_keys=False, indent=2, separators=(',', ': '))
         return js
 
-    def xml_full(self, name):
-        xml_element = ET.Element("testsuites")
+    def xml_full(self, name) -> str:
+        xml_element = ET.Element('testsuites')
         summary = self.order_summary()
-        summary_dict = dict()
+        summary_dict = {}
         for k in summary.keys():
             if k == 'total':
-                summary_dict[k] = "{:d}".format(summary[k])
+                summary_dict[k] = '{summary[k]:d}'
             else:
-                summary_dict[k.replace(" ", "")] = "{:d}".format(len(summary[k]))
+                summary_dict[k.replace(' ', '')] = f'{len(summary[k]):d}'
         summary_dict["name"] = os.path.basename(os.path.splitext(name)[0])
         summary_dict["time"] = "{:5f}".format(self.time)
         ts_element = ET.SubElement(xml_element, "testsuite", summary_dict)
         # add properties
         pr_element = ET.SubElement(ts_element, "properties")
-        prop_dict = dict()
+        prop_dict = {}
         for k in self.properties:
             prop_dict["name"] = k
             prop_dict["value"] = self.properties[k]
             p_element = ET.SubElement(pr_element, "property", prop_dict)
         # add test cases
         for test in self.test_cases:
-            tc_element = ET.SubElement(ts_element, "testcase", {'classname': test.name, 'name': test.desc, 'time': '{}'.format("{:5f}".format(test.time)if test.time is not None else "0.0")})
+            ttime = test.time if test.time is not None else 0.0
+            tc_element = ET.SubElement(ts_element, "testcase", {'classname': test.name, 'name': test.desc, 'time': f'{ttime:5f}'})
             r_element = ET.SubElement(tc_element, "pass", {"type": test.result})
             out_element = ET.SubElement(tc_element, "system-out")
             out_element.text = test.output
         return xml.dom.minidom.parseString(ET.tostring(xml_element, None, None)).toprettyxml()
 
-    def markdown_full(self, name):
+    def markdown_full(self, name: str) -> str:
         passed = []
         failed = []
         error = []
@@ -232,53 +258,51 @@ class ChipsecResults:
 
         for test in self.test_cases:
             # Test case as header level 4
-            out_string = '#### {:s}\n'.format(test.name.replace('chipsec.modules.', ''))
+            out_string = f'#### {test.name.replace("chipsec.modules.", ""):s}\n'
             for line in test.output.splitlines(True):
                 # Format output as code
                 out_string += '    {:s}'.format(line)
             destination[test.result].append(out_string)
 
-        ret_string = ""
+        ret_string = ''
         for result in destination:
             # Category as header level 1
-            ret_string += '\n# {:s}:{:d}\n'.format(result, len(destination[result]))
+            ret_string += f'\n# {result:s}:{len(destination[result]):d}\n'
             ret_string += ''.join(destination[result])
         return ret_string
 
+    def print_summary(self, runtime: Optional[bool] = None) -> None:
+        summary = self.order_summary()
+        filler = '*' * 27
+        logger().log(f'\n[CHIPSEC] {filler}  SUMMARY  {filler}')
+        if runtime:
+            logger().log(f'[CHIPSEC] Time elapsed            {runtime:.3f}')
 
-class TestCase:
-    def __init__(self, name):
-        self.name = name
-        self.result = ''
-        self.output = ''
-        self.argv = ''
-        self.desc = ''
-        self.startTime = None
-        self.endTime = None
-        self.time = None
-
-    def add_output(self, text):
-        self.output += str(text)
-        self.output += "\n"
-
-    def add_result(self, result):
-        self.result = result
-
-    def add_arg(self, arg):
-        self.argv = arg
-
-    def add_desc(self, desc):
-        self.desc = desc
-
-    def set_time(self, pTime=None):
-        """Sets the time"""
-        if pTime is not None:
-            self.time = pTime
-        elif self.startTime is None:
-            self.startTime = time.time()
-        else:
-            self.endTime = time.time()
-            self.time = self.endTime - self.startTime
-
-    def get_fields(self):
-        return {'name': self.name, 'output': self.output, 'result': self.result}
+        for k in summary.keys():
+            if k == 'total':
+                logger().log(f'[CHIPSEC] Modules {k:16}{summary[k]:d}')
+            elif k == 'warnings':
+                logger().log(f'[CHIPSEC] Modules with {k:11}{len(summary[k]):d}:')
+                for mod in summary[k]:
+                    logger().log_warning(mod)
+            elif k == 'exceptions':
+                if len(summary[k]) > 0:
+                    logger().log(f'[CHIPSEC] Modules with {k:11}{len(summary[k]):d}:')
+                    for mod in summary[k]:
+                        logger().error(mod)
+            else:
+                logger().log(f'[CHIPSEC] Modules {k:16}{len(summary[k]):d}:')
+                for mod in summary[k]:
+                    if k == 'failed to run':
+                        logger().log_error(mod)
+                    elif k == 'passed':
+                        logger().log_passed(mod)
+                    elif k == 'information':
+                        logger().log_information(mod)
+                    elif k == 'failed':
+                        logger().log_failed(mod)
+                    elif k == 'skipped':
+                        logger().log_skipped(mod)
+                    elif k == 'not applicable':
+                        logger().log_not_applicable(mod)
+        logger().log('[CHIPSEC] *****************************************************************')
