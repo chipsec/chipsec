85ca5f9e61dd4556d0748733b95b6ff8fa841239
Frinzell, Aaron <aaron.frinzell@intel.com>
2023-02-03 17:57:18 -0600

    Type Hint and fstring updates to defines.py
    Signed-off-by: Frinzell, Aaron <aaron.frinzell@intel.com>

--- chipsec/defines.py
+++ chipsec/defines.py
@@ -22,7 +22,7 @@ import struct
 import os
 import string
 import platform
-from typing import Tuple
+from typing import Dict, Tuple, Optional, AnyStr, Iterable
 
 import chipsec.file
 
@@ -121,37 +121,38 @@ MASK_32b = 0xFFFFFFFF
 MASK_64b = 0xFFFFFFFFFFFFFFFF
 
 
-def bit(bit_num):
+def bit(bit_num: int) -> int:
     return int(1 << bit_num)
 
 
-def is_set(val, bit_mask):
+def is_set(val: int, bit_mask: int) -> bool:
     return bool(val & bit_mask != 0)
 
 
-def scan_single_bit_mask(bit_mask):
+def scan_single_bit_mask(bit_mask: int) -> Optional[int]:
     for bit_num in range(0, 7):
         if bit_mask >> bit_num == 1:
             return bit_num
+    return None
 
 
-def DB(val):
+def DB(val: int) -> bytes:
     return struct.pack('<B', val)
 
 
-def DW(val):
+def DW(val: int) -> bytes:
     return struct.pack('<H', val)
 
 
-def DD(val):
+def DD(val: int) -> bytes:
     return struct.pack('<L', val)
 
 
-def DQ(val):
+def DQ(val: int) -> bytes:
     return struct.pack('<Q', val)
 
 
-SIZE2FORMAT = {
+SIZE2FORMAT: Dict[int, str] = {
     1: 'B',
     2: 'H',
     4: 'I',
@@ -159,31 +160,31 @@ SIZE2FORMAT = {
 }
 
 
-def bytestostring(mbytes):
+def bytestostring(mbytes: AnyStr) -> str:
     if isinstance(mbytes, bytes) or isinstance(mbytes, bytearray):
         return mbytes.decode("latin_1")
     else:
         return mbytes
 
 
-def stringtobytes(mstr):
+def stringtobytes(mstr: AnyStr) -> bytes:
     if isinstance(mstr, str):
         return mstr.encode("latin_1")
     else:
         return mstr
 
 
-def pack1(value, size):
+def pack1(value: int, size: int) -> bytes:
     """Shortcut to pack a single value into a string based on its size."""
     return struct.pack(SIZE2FORMAT[size], value)
 
 
-def unpack1(string, size):
+def unpack1(string: bytes, size: int) -> int:
     """Shortcut to unpack a single value from a string based on its size."""
     return struct.unpack(SIZE2FORMAT[size], string)[0]
 
 
-def get_version():
+def get_version() -> str:
     version_strs = []
     chipsec_folder = os.path.abspath(chipsec.file.get_main_dir())
     for fname in sorted([x for x in os.listdir(os.path.join(chipsec_folder, "chipsec")) if x.startswith('VERSION')]):
@@ -197,20 +198,20 @@ def os_version() -> Tuple[str, str, str, str]:
     return platform.system(), platform.release(), platform.version(), platform.machine()
 
 
-def is_printable(seq):
+def is_printable(seq) -> bool:
     return set(seq).issubset(set(string.printable))
 
 
-def is_hex(maybe_hex):
+def is_hex(maybe_hex: Iterable) -> bool:
     return all(char in string.hexdigits for char in maybe_hex)
 
 
-def is_all_ones(value, size, width=8):
+def is_all_ones(value: int, size: int, width: int = 8) -> bool:
     mask = (1 << (size * width)) - 1
     return (mask == (mask & value))
 
 
-def get_message():
+def get_message() -> str:
     msg_str = ""
     chipsec_folder = os.path.abspath(chipsec.file.get_main_dir())
     msg_file = os.path.join(chipsec_folder, "chipsec", "MESSAGE")
