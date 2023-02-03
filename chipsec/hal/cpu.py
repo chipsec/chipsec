a2867869bdf84e5ebee82875351c50a49b389f36
Frinzell, Aaron <aaron.frinzell@intel.com>
2023-02-03 17:23:45 -0600

    Type Hint and fstring updates to cpu.py
    Signed-off-by: Frinzell, Aaron <aaron.frinzell@intel.com>

--- chipsec/hal/cpu.py
+++ chipsec/hal/cpu.py
@@ -23,7 +23,7 @@
 CPU related functionality
 
 """
-
+from typing import Dict, List, Tuple, Optional
 from chipsec.hal import acpi, hal_base, paging
 from chipsec.logger import logger
 
@@ -45,32 +45,28 @@ class CPU(hal_base.HALBase):
         super(CPU, self).__init__(cs)
         self.helper = cs.helper
 
-    def read_cr(self, cpu_thread_id, cr_number):
+    def read_cr(self, cpu_thread_id: int, cr_number: int) -> int:
         value = self.helper.read_cr(cpu_thread_id, cr_number)
-        if logger().HAL:
-            logger().log("[cpu{:d}] read CR{:d}: value = 0x{:08X}".format(cpu_thread_id, cr_number, value))
+        logger().log_hal(f'[cpu{cpu_thread_id:d}] read CR{cr_number:d}: value = 0x{value:08X}')
         return value
 
-    def write_cr(self, cpu_thread_id, cr_number, value):
-        if logger().HAL:
-            logger().log("[cpu{:d}] write CR{:d}: value = 0x{:08X}".format(cpu_thread_id, cr_number, value))
+    def write_cr(self, cpu_thread_id: int, cr_number: int, value: int) -> int:
+        logger().log_hal(f'[cpu{cpu_thread_id:d}] write CR{cr_number:d}: value = 0x{value:08X}')
         status = self.helper.write_cr(cpu_thread_id, cr_number, value)
         return status
 
-    def cpuid(self, eax, ecx):
-        if logger().HAL:
-            logger().log("[cpu] CPUID in : EAX=0x{:08X}, ECX=0x{:08X}".format(eax, ecx))
+    def cpuid(self, eax: int, ecx: int) -> Tuple[int, int, int, int]:
+        logger().log_hal(f'[cpu] CPUID in : EAX=0x{eax:08X}, ECX=0x{ecx:08X}')
         (eax, ebx, ecx, edx) = self.helper.cpuid(eax, ecx)
-        if logger().HAL:
-            logger().log("[cpu] CPUID out: EAX=0x{:08X}, EBX=0x{:08X}, ECX=0x{:08X}, EDX=0x{:08X}".format(eax, ebx, ecx, edx))
+        logger().log_hal(f'[cpu] CPUID out: EAX=0x{eax:08X}, EBX=0x{ebx:08X}, ECX=0x{ecx:08X}, EDX=0x{edx:08X}')
         return (eax, ebx, ecx, edx)
 
     # Using cpuid check if running under vmm control
-    def check_vmm(self):
+    def check_vmm(self) -> int:
         # check Hypervisor Present
-        (eax, ebx, ecx, edx) = self.cpuid(0x01, 0)
+        (_, ebx, ecx, edx) = self.cpuid(0x01, 0)
         if (ecx & 0x80000000):
-            (eax, ebx, ecx, edx) = self.cpuid(0x40000000, 0)
+            (_, ebx, ecx, edx) = self.cpuid(0x40000000, 0)
             is_xen = ((ebx == 0x566e6558) and (ecx == 0x65584d4d) and (edx == 0x4d4d566e))
             if is_xen:
                 return VMM_XEN
@@ -86,32 +82,32 @@ class CPU(hal_base.HALBase):
         return VMM_NONE
 
     # Using CPUID we can determine if Hyper-Threading is enabled in the CPU
-    def is_HT_active(self):
+    def is_HT_active(self) -> bool:
         logical_processor_per_core = self.get_number_logical_processor_per_core()
         return logical_processor_per_core > 1
 
     # Using the CPUID we determine the number of logical processors per core
-    def get_number_logical_processor_per_core(self):
-        (eax, ebx, ecx, edx) = self.cpuid(0x0b, 0x0)
+    def get_number_logical_processor_per_core(self) -> int:
+        (_, ebx, _, _) = self.cpuid(0x0b, 0x0)
         return ebx
 
     # Using CPUID we can determine the number of logical processors per package
-    def get_number_logical_processor_per_package(self):
-        (eax, ebx, ecx, edx) = self.cpuid(0x0b, 0x1)
+    def get_number_logical_processor_per_package(self) -> int:
+        (_, ebx, _, _) = self.cpuid(0x0b, 0x1)
         return ebx
 
     # Using CPUID we can determine the number of physical processors per package
-    def get_number_physical_processor_per_package(self):
+    def get_number_physical_processor_per_package(self) -> int:
         logical_processor_per_core = self.get_number_logical_processor_per_core()
         logical_processor_per_package = self.get_number_logical_processor_per_package()
         return (logical_processor_per_package // logical_processor_per_core)
 
     # determine number of logical processors in the core
-    def get_number_threads_from_APIC_table(self):
+    def get_number_threads_from_APIC_table(self) -> int:
         _acpi = acpi.ACPI(self.cs)
         dACPIID = {}
-        for apic in _acpi.get_parse_ACPI_table(acpi.ACPI_TABLE_SIG_APIC):
-            table_header, APIC_object, table_header_blob, table_blob = apic
+        for apic in _acpi.get_parse_ACPI_table(acpi.ACPI_TABLE_SIG_APIC):  # (table_header, APIC_object, table_header_blob, table_blob)
+            _, APIC_object, _, _ = apic
             for structure in APIC_object.apic_structs:
                 if 0x00 == structure.Type:
                     if not structure.ACICID in dACPIID:
@@ -120,37 +116,34 @@ class CPU(hal_base.HALBase):
         return len(dACPIID)
 
     # determine the cpu threads location within a package/core
-    def get_cpu_topology(self):
+    def get_cpu_topology(self) -> Dict[str, Dict[int, List[int]]]:
         num_threads = self.cs.helper.get_threads_count()
-        packages = {}
-        cores = {}
+        packages: Dict[int, List[int]] = {}
+        cores: Dict[int, List[int]] = {}
         for thread in range(num_threads):
-            if logger().HAL:
-                self.logger.log("Setting affinity to: {:d}".format(thread))
+            self.logger.log_hal(f'Setting affinity to: {thread:d}')
             self.cs.helper.set_affinity(thread)
             eax = 0xb   # cpuid leaf 0B contains x2apic info
             ecx = 1     # ecx 1 will get us pkg_id in edx after shifting right by _eax
-            (_eax, _ebx, _ecx, _edx) = self.cs.cpu.cpuid(eax, ecx)
+            (_eax, _, _, _edx) = self.cs.cpu.cpuid(eax, ecx)
             pkg_id = _edx >> (_eax & 0xf)
             if pkg_id not in packages:
                 packages[pkg_id] = []
             packages[pkg_id].append(thread)
 
             ecx = 0     # ecx 0 will get us the core_id in edx after shifting right by _eax
-            (_eax, _ebx, _ecx, _edx) = self.cs.cpu.cpuid(eax, ecx)
+            (_eax, _, _, _edx) = self.cs.cpu.cpuid(eax, ecx)
             core_id = _edx >> (_eax & 0xf)
             if core_id not in cores:
                 cores[core_id] = []
             cores[core_id].append(thread)
-            if logger().HAL:
-                self.logger.log("pkg id is {:x}".format(pkg_id))
-            if logger().HAL:
-                self.logger.log("core id is {:x}".format(core_id))
+            self.logger.log_hal(f'pkg id is {pkg_id:x}')
+            self.logger.log_hal(f'core id is {core_id:x}')
         topology = {'packages': packages, 'cores': cores}
         return topology
 
     # determine number of physical sockets using the CPUID and APIC ACPI table
-    def get_number_sockets_from_APIC_table(self):
+    def get_number_sockets_from_APIC_table(self) -> int:
         number_threads = self.get_number_threads_from_APIC_table()
         logical_processor_per_package = self.get_number_logical_processor_per_package()
         return (number_threads // logical_processor_per_package)
@@ -158,7 +151,7 @@ class CPU(hal_base.HALBase):
     #
     # Return SMRR MSR physical base and mask
     #
-    def get_SMRR(self):
+    def get_SMRR(self) -> Tuple[int, int]:
         smrambase = self.cs.read_register_field('IA32_SMRR_PHYSBASE', 'PhysBase', True)
         smrrmask = self.cs.read_register_field('IA32_SMRR_PHYSMASK', 'PhysMask', True)
         return (smrambase, smrrmask)
@@ -166,7 +159,7 @@ class CPU(hal_base.HALBase):
     #
     # Return SMRAM region base, limit and size as defined by SMRR
     #
-    def get_SMRR_SMRAM(self):
+    def get_SMRR_SMRAM(self) -> Tuple[int, int, int]:
         (smram_base, smrrmask) = self.get_SMRR()
         smram_base &= smrrmask
         smram_size = ((~smrrmask) & 0xFFFFFFFF) + 1
@@ -176,7 +169,7 @@ class CPU(hal_base.HALBase):
     #
     # Returns TSEG base, limit and size
     #
-    def get_TSEG(self):
+    def get_TSEG(self) -> Tuple[int, int, int]:
         if self.cs.is_server():
             # tseg register has base and limit
             tseg_base = self.cs.read_register_field('TSEG_BASE', 'base', preserve_field_position=True)
@@ -194,27 +187,29 @@ class CPU(hal_base.HALBase):
     #
     # Returns SMRAM base from either SMRR MSR or TSEG PCIe config register
     #
-    def get_SMRAM(self):
+    def get_SMRAM(self) -> Tuple[int, int, int]:
         smram_base = None
         smram_limit = None
         smram_size = 0
         try:
-            if (self.check_SMRR_supported()):
+            if self.check_SMRR_supported():
                 (smram_base, smram_limit, smram_size) = self.get_SMRR_SMRAM()
-        except:
-            pass
+        except Exception:
+            self.logger.log_hal('[cpu] Error using get_SMRR_SMRAM() to get SMRAM!')
 
-        if smram_base is None:
+        if (smram_base is None) or (smram_limit is None):
             try:
                 (smram_base, smram_limit, smram_size) = self.get_TSEG()
-            except:
-                pass
+            except Exception:
+                self.logger.log_hal('[cpu] Error using get_TSEG() to get SMRAM!')
+                smram_base = 0
+                smram_limit = 0
         return (smram_base, smram_limit, smram_size)
 
     #
     # Check that SMRR is supported by CPU in IA32_MTRRCAP_MSR[SMRR]
     #
-    def check_SMRR_supported(self):
+    def check_SMRR_supported(self) -> bool:
         mtrrcap_msr_reg = self.cs.read_register('MTRRCAP')
         if logger().HAL:
             self.cs.print_register('MTRRCAP', mtrrcap_msr_reg)
@@ -224,22 +219,20 @@ class CPU(hal_base.HALBase):
     #
     # Dump CPU page tables at specified physical base of paging-directory hierarchy (CR3)
     #
-    def dump_page_tables(self, cr3, pt_fname=None):
+    def dump_page_tables(self, cr3: int, pt_fname: Optional[str] = None) -> None:
         _orig_logname = logger().LOG_FILE_NAME
         hpt = paging.c_ia32e_page_tables(self.cs)
-        if logger().HAL:
-            logger().log('[cpu] dumping paging hierarchy at physical base (CR3) = 0x{:08X}...'.format(cr3))
+        logger().log_hal(f'[cpu] dumping paging hierarchy at physical base (CR3) = 0x{cr3:08X}...')
         if pt_fname is None:
-            pt_fname = ('pt_{:08X}'.format(cr3))
+            pt_fname = f'pt_{cr3:08X}'
         logger().set_log_file(pt_fname)
         hpt.read_pt_and_show_status(pt_fname, 'PT', cr3)
         logger().set_log_file(_orig_logname)
         if hpt.failure:
             logger().log_error('could not dump page tables')
 
-    def dump_page_tables_all(self):
+    def dump_page_tables_all(self) -> None:
         for tid in range(self.cs.msr.get_cpu_thread_count()):
             cr3 = self.read_cr(tid, 3)
-            if logger().HAL:
-                logger().log('[cpu{:d}] found paging hierarchy base (CR3): 0x{:08X}'.format(tid, cr3))
+            logger().log_hal(f'[cpu{tid:d}] found paging hierarchy base (CR3): 0x{cr3:08X}')
             self.dump_page_tables(cr3)
