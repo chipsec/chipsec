e0095d523f5721bda0d0d3eab151f34e4b0b7e01
Aaron Frinzell <aaron.frinzell@intel.com>
2023-03-20 19:35:12 -0500

    Update chipsec/helper/dal/dalhelper.py
    Co-authored-by: dscott90 <dan.scott@intel.com>

--- chipsec/helper/dal/dalhelper.py
+++ chipsec/helper/dal/dalhelper.py
@@ -241,7 +241,7 @@ class DALHelper(Helper):
     def read_cr(self, cpu_thread_id: int, cr_number: int) -> int:
         if not self.base.threads[cpu_thread_id].isenabled:
             en_thread = self.find_thread()
-            logger().log_debug('[WARNING] Selected thread [{cpu_thread_id:d}] was disabled, using [{en_thread:d}].')
+            logger().log_debug(f'[WARNING] Selected thread [{cpu_thread_id:d}] was disabled, using [{en_thread:d}].')
             cpu_thread_id = en_thread
         if cr_number == 0:
             val = self.base.threads[cpu_thread_id].state.regs.cr0.value
