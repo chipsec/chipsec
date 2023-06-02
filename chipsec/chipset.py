chipsec/chipsec

commit 2691f50bae8a6314f7ec0884ab3a8b83a02fdeb1

Detect PCH based on platform vendor ID
An alternative approach would be to use get_cpuid(eax=0) to recover the
vendor, but cpuid is only available when a driver is loaded. Assume that
there is always a PCI device 00:0.0 providing the correct vendor.

Currently supports AMD and Intel.

Bug: #825
Signed-off-by: Gabriel Kerneis <gabriel.kerneis@ssi.gouv.fr>

chipsec/chipset.py
@@ -89,17 +89,17 @@ CHIPSET_FAMILY_QUARK = []
 
 PCH_CODE_PREFIX = 'PCH_'
 
+PCH_ADDRESS = {
+    # Intel: 0:1F.0
+    0x8086: (0, 0x1F, 0),
+    # AMD: 0:14.3
+    0x1022: (0, 0x14, 3)
+}
+
 try:
     from chipsec.custom_chipsets import *
 except ImportError:
     pass
-
-
-def f_xml(self, x):
-    XMLFILE_RE = re.compile("^\w+\.xml")
-    return ( x.find('common') == -1 and XMLFILE_RE.match(x) )
-def map_xmlname(self, x):
-    return x.split('.')[0]
 
 
 class Chipset:
@@ -167,13 +167,17 @@ class Chipset:
             rid = self.pci.read_byte(0, 0, 0, PCI_HDR_RID_OFF)
         except:
             if logger().DEBUG: logger().error("pci.read_dword couldn't read platform VID/DID")
-        try:
-            vid_did = self.pci.read_dword(0, 31, 0, 0)
-            pch_vid = vid_did & 0xFFFF
-            pch_did = (vid_did >> 16) & 0xFFFF
-            pch_rid = self.pci.read_byte(0, 31, 0, PCI_HDR_RID_OFF)
-        except:
-            if logger().DEBUG: logger().error("pci.read_dword couldn't read PCH VID/DID")
+        if not vid in PCH_ADDRESS:
+            if logger().DEBUG: logger().error("PCH address unknown for VID {}.".format(vid))
+        else:
+            try:
+                 (bus,dev,fun) = PCH_ADDRESS[vid]
+                 vid_did = self.pci.read_dword(bus, dev, fun, 0)
+                 pch_vid = vid_did & 0xFFFF
+                 pch_did = (vid_did >> 16) & 0xFFFF
+                 pch_rid = self.pci.read_byte(0, 31, 0, PCI_HDR_RID_OFF)
+            except:
+                 if logger().DEBUG: logger().error("pci.read_dword couldn't read PCH VID/DID")
         return (vid, did, rid, pch_vid, pch_did, pch_rid)
 
     def get_cpuid(self):

commit 5e76b2399f3bd33c0f98d92fb28c982b53bc1a39

Read the AMD LPC revision ID using the correct BDF address
On AMD, the "PCH" is the LPC device on PCI 0:14.3. According to "PPR for
AMD Family 17h Model 18h B1. 55570-B1 Rev 3.16 - Apr 14, 2021."
available on
https://www.amd.com/en/support/tech-docs/processor-programming-reference-ppr-for-amd-family-17h-model-18h-revision-b1
the low byte at D14F3x008 (bus 0 device 14 function 3 offset 8) is the
RevisionID of this device.

Using 0:1F.0 to read the RID in `Chipset.detect_platform` is likely to
be a bug from commit 2691f50bae8a ("Detect PCH based on platform vendor
ID"), which introduced AMD support in this function. Fix this by using
`bus, dev, fun` to read `pch_rid`.

Signed-off-by: Nicolas Iooss <nicolas.iooss_git@polytechnique.org>

chipsec/chipset.py
@@ -180,7 +180,7 @@ class Chipset:
                 vid_did = self.pci.read_dword(bus, dev, fun, 0)
                 pch_vid = vid_did & 0xFFFF
                 pch_did = (vid_did >> 16) & 0xFFFF
-                pch_rid = self.pci.read_byte(0, 31, 0, PCI_HDR_RID_OFF)
+                pch_rid = self.pci.read_byte(bus, dev, fun, PCI_HDR_RID_OFF)
             except:
                 if logger().DEBUG:
                     logger().log_error("pci.read_dword couldn't read PCH VID/DID")