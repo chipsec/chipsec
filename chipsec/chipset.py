4637494190460bac0f85128261f9e3a65b4da4ec
Nathaniel Mitchell <nathaniel.p.mitchell@intel.com>
2024-01-05 11:19:35 -0800

    Print pci devices when using verbose output
    Signed-off-by: Nathaniel Mitchell <nathaniel.p.mitchell@intel.com>

--- chipsec/chipset.py
+++ chipsec/chipset.py
@@ -157,13 +157,14 @@ class Chipset:
                 if logger().DEBUG:
                     logger().log("[*] Discovering Bus Configuration:")
             if _unknown_pch:
-                pci.print_pci_devices(self.pci.enumerate_devices())
                 msg = 'Unknown PCH: VID = 0x{:04X}, DID = 0x{:04X}, RID = 0x{:02X}'.format(self.Cfg.pch_vid, self.Cfg.pch_did, self.Cfg.pch_rid)
                 if self.Cfg.is_pch_req() and start_helper:
                     logger().log_error("Chipset requires a supported PCH to be loaded. {}".format(msg))
                     raise UnknownChipsetError(msg)
                 else:
                     logger().log("[!]       {}; Using Default.".format(msg))
+        if start_helper and ((logger().VERBOSE) or (load_config and (_unknown_pch or _unknown_proc))):
+            pci.print_pci_devices(self.pci.enumerate_devices())
         if _unknown_pch or _unknown_proc:
             msg = 'Results from this system may be incorrect.'
             logger().log("[!]            {}".format(msg))
