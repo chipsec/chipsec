662123a2ba9ab280bab4e8c652673a695a9456b7
Nathaniel Mitchell <nathaniel.p.mitchell@intel.com>
2024-06-03 16:36:52 -0700

    Update chipsec/modules/tools/secureboot/te.py
    Co-authored-by: dscott90 <dan.scott@intel.com>

--- chipsec/modules/tools/secureboot/te.py
+++ chipsec/modules/tools/secureboot/te.py
@@ -518,7 +518,7 @@ class te(BaseModule):
             if not os.path.exists(file_path):
                 self.logger.log_error(f'Cannot find file \'{file_path}\'')
                 self.logger.log_error(f'Please download it from https://github.com/chipsec/chipsec/releases/download/binaries/Shell.efi ')
-                self.logger.log_error(f'And put file to \'{file_path}\'')
+                self.logger.log_error(f'And move file to \'{file_path}\'')
                 self.result.setStatusBit(self.result.status.ACCESS_RW)
                 return self.result.getReturnCode(ModuleResult.ERROR)
 
