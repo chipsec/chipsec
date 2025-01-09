404e0a876e417a1dcc9e58147bd06a862635a36c
Dan Scott <dan.scott@intel.com>
2025-01-09 14:02:24 -0800

    Adding config file for CI

--- chipsec_tools/log_parser/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-FLASK_ENV=development flask run --host 0.0.0.0
--- drivers/windows/chipsec/sign/sign64_sys.bat
+++
@@ -1,2 +0,0 @@
-copy /Y ..\..\..\chipsec\helper\windows\windows_amd64\chipsec_hlpr.sys 
-SignTool sign /fdws /t http://timestamp.digicert.com   chipsec_hlpr.sys
