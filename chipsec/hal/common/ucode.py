e6578f2a5d754e301e13e853acbd5b3eefb02a72
brentholtsclaw <brent.holtsclaw@intel.com>
2025-02-12 11:56:51 -0800

    /dev/chipsec2 ucode hal update
    Signed-off-by: brentholtsclaw <brent.holtsclaw@intel.com>

--- chipsec/hal/common/ucode.py
+++ chipsec/hal/common/ucode.py
@@ -28,18 +28,18 @@ usage:
     >>> dump_ucode_update_header( 'ucode.pdb' )
 """
 
-import struct
+from collections import namedtuple
 import os
+import struct
 from typing import AnyStr
-from chipsec.library.logger import logger
+from chipsec.hal import hal_base
 from chipsec.library.file import read_file
 
 IA32_MSR_BIOS_UPDT_TRIG = 0x79
 IA32_MSR_BIOS_SIGN_ID = 0x8B
 IA32_MSR_BIOS_SIGN_ID_STATUS = 0x1
 
-
-from collections import namedtuple
+UCODE_HEADER_SIZE = 0x30
 
 
 class UcodeUpdateHeader(namedtuple('UcodeUpdateHeader', 'header_version update_revision date processor_signature checksum loader_revision processor_flags data_size total_size reserved1 reserved2 reserved3')):
@@ -64,64 +64,60 @@ Reserved3           : 0x{self.reserved3:08X}
 """
 
 
-UCODE_HEADER_SIZE = 0x30
-
-
-def dump_ucode_update_header(pdb_ucode_buffer: bytes) -> UcodeUpdateHeader:
-    ucode_header = UcodeUpdateHeader(*struct.unpack_from('12I', pdb_ucode_buffer))
-    logger().log_hal(str(ucode_header))
-    return ucode_header
-
-
-def read_ucode_file(ucode_filename: str) -> bytes:
-    ucode_buf = read_file(ucode_filename)
-    if (ucode_filename.endswith('.pdb')):
-        logger().log_hal(f"[ucode] PDB file '{ucode_filename:256}' has ucode update header (size = 0x{UCODE_HEADER_SIZE:X})")
-        dump_ucode_update_header(ucode_buf)
-        return ucode_buf[UCODE_HEADER_SIZE:]
-    else:
-        return ucode_buf
-
-
-class Ucode: #TODO Refactor and have it derive from halbase
+class Ucode(hal_base.HALBase):
     def __init__(self, cs):
         self.helper = cs.helper
         self.cs = cs
 
-    # @TODO remove later/replace with msr.get_cpu_thread_count()
-    def get_cpu_thread_count(self) -> int:
-        thread_count = self.cs.register.read_field("IA32_MSR_CORE_THREAD_COUNT", "Thread_Count")
-        return thread_count
-
     def ucode_update_id(self, cpu_thread_id: int) -> int:
-        (bios_sign_id_lo, bios_sign_id_hi) = self.helper.read_msr(cpu_thread_id, IA32_MSR_BIOS_SIGN_ID)
+        (bios_sign_id_lo, bios_sign_id_hi) = self.cs.hals.Msr.read_msr(cpu_thread_id, IA32_MSR_BIOS_SIGN_ID)
         ucode_update_id = bios_sign_id_hi
 
         if (bios_sign_id_lo & IA32_MSR_BIOS_SIGN_ID_STATUS):
-            logger().log_hal(f'[ucode] CPU{cpu_thread_id:d}: last Microcode update failed (current microcode id = 0x{ucode_update_id:08X})')
+            self.logger.log_hal(f'[ucode] CPU{cpu_thread_id:d}: last Microcode update failed (current microcode id = 0x{ucode_update_id:08X})')
         else:
-            logger().log_hal(f'[ucode] CPU{cpu_thread_id:d}: Microcode update ID = 0x{ucode_update_id:08X}')
+            self.logger.log_hal(f'[ucode] CPU{cpu_thread_id:d}: Microcode update ID = 0x{ucode_update_id:08X}')
 
         return ucode_update_id
 
     def update_ucode_all_cpus(self, ucode_file: str) -> bool:
         if not (os.path.exists(ucode_file) and os.path.isfile(ucode_file)):
-            logger().log_error(f"Ucode file not found: '{ucode_file:.256}'")
+            self.logger.log_error(f"Ucode file not found: '{ucode_file:.256}'")
             return False
-        ucode_buf = read_ucode_file(ucode_file)
+        ucode_buf = self.read_ucode_file(ucode_file)
         if (ucode_buf is not None) and (len(ucode_buf) > 0):
-            for tid in range(self.get_cpu_thread_count()):
+            for tid in range(self.cs.hals.Msr.get_cpu_thread_count()):
                 self.load_ucode_update(tid, ucode_buf)
         return True
 
     def update_ucode(self, cpu_thread_id: int, ucode_file: str) -> int:
         if not (os.path.exists(ucode_file) and os.path.isfile(ucode_file)):
-            logger().log_error(f"Ucode file not found: '{ucode_file:.256}'")
+            self.logger.log_error(f"Ucode file not found: '{ucode_file:.256}'")
             return False
-        _ucode_buf = read_ucode_file(ucode_file)
+        _ucode_buf = self.read_ucode_file(ucode_file)
         return self.load_ucode_update(cpu_thread_id, _ucode_buf)
 
     def load_ucode_update(self, cpu_thread_id: int, ucode_buf: AnyStr) -> int:
-        logger().log_hal(f'[ucode] Loading microcode update on CPU{cpu_thread_id:d}')
+        self.logger.log_hal(f'[ucode] Loading microcode update on CPU{cpu_thread_id:d}')
         self.helper.load_ucode_update(cpu_thread_id, ucode_buf)
         return self.ucode_update_id(cpu_thread_id)
+
+    def dump_ucode_update_header(self, pdb_ucode_buffer: bytes) -> UcodeUpdateHeader:
+        if len(pdb_ucode_buffer) < UCODE_HEADER_SIZE:
+            self.logger.log('Buffer is too small')
+            return None
+        ucode_header = UcodeUpdateHeader(*struct.unpack_from('12I', pdb_ucode_buffer))
+        self.logger.log_hal(str(ucode_header))
+        return ucode_header
+
+    def read_ucode_file(self, ucode_filename: str) -> bytes:
+        ucode_buf = read_file(ucode_filename)
+        if (ucode_filename.endswith('.pdb')):
+            self.logger.log_hal(f"[ucode] PDB file '{ucode_filename:256}' has ucode update header (size = 0x{UCODE_HEADER_SIZE:X})")
+            self.dump_ucode_update_header(ucode_buf)
+            return ucode_buf[UCODE_HEADER_SIZE:]
+        else:
+            return ucode_buf
+
+
+haldata = {"arch": ['FFFF'], 'name': ['Ucode']}
