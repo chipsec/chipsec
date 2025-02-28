chipsec/chipsec

commit d5b9f4018a6f0c0e744342579bdf45eb9ef6bad1

fix for flake8 rule E231
This commit fixes all the E231 errors.  They were found by running "flake8 ." from the root directory.  E231 is an error defined by "pycodestyle", and has no functional impact to the source.  This is simply fixing whitespace as defined by CHIPSEC's .flake8 configuration.

background:
https://www.flake8rules.com/rules/E231.html

Signed-off-by: William Leara <william.leara@dell.com>

chipsec/library/register.py
@@ -550,7 +550,7 @@ class Register:
         rtype = reg['type']
         reg_str = ''
         reg_width = reg["size"] * 2
-        reg_val_str = f'0x{reg_val:0{reg_width:d}X}'
+        reg_val_str = f'0x{reg_val:0{reg_width: d}X}'
         if RegisterType.PCICFG == rtype or RegisterType.MMCFG == rtype:
             if bus is not None:
                 b = bus

chipsec/hal/psp.py
@@ -40,12 +40,12 @@ class PSP:
         self.helper = cs.helper
 
     def smu_read32(self, reg):
-        self.cs.pci.write_dword(0,0,0,self.SMN_INDEX_ADDR,reg)
-        return self.cs.pci.read_dword(0,0,0,self.SMN_DATA_ADDR)
+        self.cs.pci.write_dword(0, 0, 0, self.SMN_INDEX_ADDR, reg)
+        return self.cs.pci.read_dword(0, 0, 0, self.SMN_DATA_ADDR)
 
     def smu_write32(self, reg, val):
-        self.cs.pci.write_dword(0,0,0,self.SMN_INDEX_ADDR,reg)
-        return self.cs.pci.write_dword(0,0,0, self.SMN_DATA_ADDR,val)
+        self.cs.pci.write_dword(0, 0, 0, self.SMN_INDEX_ADDR, reg)
+        return self.cs.pci.write_dword(0, 0, 0, self.SMN_DATA_ADDR, val)
 
     def psp_mbox_command(self, cmd):
         #  Command ID (bits [23:16]), Status (bits [15:0]) fields and Ready flag (bit #31)
@@ -71,11 +71,11 @@ class PSP:
             logger().log_bad(f'Timeout polling for PSP Mailbox Ready (Idle)')
             return [0xbaddbadd]
 
-        (buf_va,buf_pa) = self.helper.alloc_phys_mem(buf_size,0x1000_0000_0000)
+        (buf_va, buf_pa) = self.helper.alloc_phys_mem(buf_size, 0x1000_0000_0000)
 
         # send physical address for msg buffer
-        self.smu_write32(self.SMU_PSP_MBOX_CMD_BUF_LO,buf_pa & 0xFFFFFFFF)
-        self.smu_write32(self.SMU_PSP_MBOX_CMD_BUF_HI,(buf_pa >> 32) & 0xFFFFFFFF)
+        self.smu_write32(self.SMU_PSP_MBOX_CMD_BUF_LO, buf_pa & 0xFFFFFFFF)
+        self.smu_write32(self.SMU_PSP_MBOX_CMD_BUF_HI, (buf_pa >> 32) & 0xFFFFFFFF)
 
         # send command
         mbox_cmd_status_value = (cmd << 16) & 0x00ff0000
@@ -97,8 +97,8 @@ class PSP:
             return [0xbaddbadd]
         
         buffer = []
-        for i in range(0,num_buf):
-            buffer.append(int.from_bytes(self.helper.read_phys_mem(buf_pa + (i * dword_size),dword_size),'little'))
+        for i in range(0, num_buf):
+            buffer.append(int.from_bytes(self.helper.read_phys_mem(buf_pa + (i * dword_size), dword_size), 'little'))
 
         self.helper.free_phys_mem(buf_va)
 

chipsec/hal/uefi_common.py
@@ -217,7 +217,7 @@ def EFI_GUID_STR(guid: bytes) -> str:
     return str(guid_str).upper()
 
 
-def align(of:int, size: int) -> int:
+def align(of: int, size: int) -> int:
     of = (((of + size - 1) // size) * size)
     return of
 

chipsec/helper/efi/efihelper.py
@@ -181,7 +181,7 @@ class EfiHelper(Helper):
         else:
             return edk2.readpci(bus, device, function, address, size)
 
-    def write_pci_reg(self, bus: int, device: int, function: int, address:int, value: int, size: int) -> int:
+    def write_pci_reg(self, bus: int, device: int, function: int, address: int, value: int, size: int) -> int:
         return edk2.writepci(bus, device, function, address, value, size)
 
     #

chipsec/helper/linux/linuxhelper.py
@@ -393,7 +393,7 @@ class LinuxHelper(Helper):
         in_buf = struct.pack(f'3{self._pack}', phys_address, size, value)
         out_buf = self.ioctl(IOCTL_WRMMIO, in_buf)
 
-    def get_ACPI_table(self, table_name:str) -> Optional['Array']:
+    def get_ACPI_table(self, table_name: str) -> Optional['Array']:
         raise UnimplementedAPIError('get_ACPI_table')
 
     def enum_ACPI_tables(self) -> Optional['Array']:

chipsec/helper/record/recordhelper.py
@@ -135,7 +135,7 @@ class RecordHelper(Helper):
     #
     # physical_address is 64 bit integer
     #
-    def read_phys_mem(self, phys_address:int, length:int) -> bytes:
+    def read_phys_mem(self, phys_address: int, length: int) -> bytes:
         return self._call_subhelper(phys_address, length)
 
     def write_phys_mem(self, phys_address: int, length: int, buf: bytes) -> int:

chipsec/modules/common/cpu/cpu_info.py
@@ -107,7 +107,7 @@ class cpu_info(BaseModule):
         # Determine number of threads to check
         thread_count = 1
         if not self.cs.os_helper.is_efi():
-            (_, _, r_rcx, _) = self.cs.cpu.cpuid(0x80000008,0)
+            (_, _, r_rcx, _) = self.cs.cpu.cpuid(0x80000008, 0)
             thread_count = r_rcx & 0xff
 
         for thread in range(thread_count):
@@ -125,9 +125,9 @@ class cpu_info(BaseModule):
             self.logger.log(f'[*] Processor: {brand}')
 
             # "Authentic AMD"
-            e_rbx = int("htuA".encode('utf-8').hex(),16)    #0x68747541
-            e_rcx = int("DMAc".encode('utf-8').hex(),16)    #0x444D4163
-            e_rdx = int("itne".encode('utf-8').hex(),16)    #0x69746E65
+            e_rbx = int("htuA".encode('utf-8').hex(), 16)    #0x68747541
+            e_rcx = int("DMAc".encode('utf-8').hex(), 16)    #0x444D4163
+            e_rdx = int("itne".encode('utf-8').hex(), 16)    #0x69746E65
             (_, r_rbx, r_rcx, r_rdx) = self.cs.cpu.cpuid(0x00, 0x00)
 
             if not(e_rbx == r_rbx and e_rcx == r_rcx and e_rdx == r_rdx):

chipsec/modules/common/cpu/spectre_v2.py
@@ -295,7 +295,7 @@ class spectre_v2(BaseModule):
 
         elif self.cs.is_amd():
             res = ModuleResult.PASSED
-            fields = ['IBRS','STIBP','SSBD','PSFD']
+            fields = ['IBRS', 'STIBP', 'SSBD', 'PSFD']
             settings = {} 
             threads = {}
             fail = False
@@ -311,7 +311,7 @@ class spectre_v2(BaseModule):
             if fail:
                 self.logger.log_failed("Spectre V2 Mitigations Currently not enabled")
                 res = ModuleResult.PASSED
-                for t,s in threads.items():
+                for t, s in threads.items():
                     self.logger.log_warning(f"Thread[{t}]: {fields[0]}:{s[fields[0]]} {fields[1]}:{s[fields[1]]} {fields[2]}:{s[fields[2]]} {fields[3]}:{s[fields[3]]}")
             else:
                 self.logger.log_passed("Spectre V2 Mitigations Currently enabled")

commit a0ba47efce3c3fb4d3c681ce63bd1354398af70d

remove space in formatted string
A bug introduced in d5b9f401, and found by @npmitche in PR #2413.

Signed-off-by: William Leara <william.leara@dell.com>

chipsec/library/register.py
@@ -550,7 +550,7 @@ class Register:
         rtype = reg['type']
         reg_str = ''
         reg_width = reg["size"] * 2
-        reg_val_str = f'0x{reg_val:0{reg_width: d}X}'
+        reg_val_str = f'0x{reg_val:0{reg_width:d}X}'
         if RegisterType.PCICFG == rtype or RegisterType.MMCFG == rtype:
             if bus is not None:
                 b = bus