59b00904a7e1eb39bc42faa10eef68a9f86afe4f
Nathaniel Mitchell <nathaniel.p.mitchell@intel.com>
2025-03-04 22:13:58 -0800

    Extract process_config_complex and bar parsing
    Signed-off-by: Nathaniel Mitchell <nathaniel.p.mitchell@intel.com>

--- chipsec/cfg/parsers/core_parsers.py
+++ chipsec/cfg/parsers/core_parsers.py
@@ -38,6 +38,7 @@ from chipsec.cfg.parsers.registers.mmio import MMIORegisters #
 from chipsec.cfg.parsers.registers.msgbus import MSGBUSRegisters #
 from chipsec.cfg.parsers.registers.msr import MSRRegisters #
 from chipsec.cfg.parsers.registers.pci import PCIRegisters #pcicfg
+from chipsec.cfg.parsers.core_parser_helper import config_convert_data as _config_convert_data, CoreParserHelper
 from chipsec.parsers import BaseConfigParser
 from chipsec.parsers import Stage
 from chipsec.parsers import info_data, config_data
@@ -47,53 +48,9 @@ from chipsec.library.strings import make_hex_key_str
 CONFIG_TAG = 'configuration'
 
 
-def _get_range_data(xml_node, attr):
-    int_items = []
-    for item in xml_node.attrib[attr].split(','):
-        item = item.strip()
-        if item.upper().endswith('*'):
-            x = int(item.replace('*', '0'), 0)
-            int_items.extend(range(x, x + 0x10))
-        elif '-' in item:
-            item_min, item_max = item.split('-', 1)
-            int_items.extend(range(int(item_min, 0), int(item_max, 0) + 1))
-        else:
-            int_items.append(int(item, 0))
-    return int_items
 
 
-def _config_convert_data(xml_node, did_is_range=False):
-    # INT_KEYS = ['dev', 'fun', 'vid', 'did', 'rid', 'offset',
-    #             'bit', 'size', 'port', 'msr', 'value', 'address',
-    #             'fixed_address', 'base_align', 'align_bits', 'mask',
-    #             'reg_align', 'limit_align', 'regh_align',
-    #             'width', 'reg']
-    INT_KEYS = ['dev', 'fun', 'vid', 'did', 'rid', 'offset',
-                'bit', 'size', 'port', 'msr', 'value', 'address',
-                'fixed_address', 'base_align', 'align_bits', 'mask',
-                'reg_align', 'limit_align', 'regh_align', 'default', 'limit']
-    BOOL_KEYS = ['req_pch']
-    INT_LIST_KEYS = ['bus']
-    STR_LIST_KEYS = ['config']
-    RANGE_LIST_KEYS = ['detection_value']
-    if did_is_range:
-        INT_KEYS.remove('did')
-        RANGE_LIST_KEYS.append('did')
-    node_data = {}
-    for key in xml_node.attrib:
-        if key in INT_KEYS:
-            node_data[key] = int(xml_node.attrib[key], 0)
-        elif key in INT_LIST_KEYS:
-            node_data[key] = [int(xml_node.attrib[key], 0)]
-        elif key in STR_LIST_KEYS:
-            node_data[key] = [x.strip() for x in xml_node.attrib[key].split(',')]
-        elif key in RANGE_LIST_KEYS:
-            node_data[key] = _get_range_data(xml_node, key)
-        elif key in BOOL_KEYS:
-            node_data[key] = xml_node.attrib[key].lower() == 'true'
-        else:
-            node_data[key] = xml_node.attrib[key]
-    return node_data
+
 
 
 class PlatformInfo(BaseConfigParser):
@@ -153,6 +110,11 @@ class PlatformInfo(BaseConfigParser):
 
 
 class DevConfig(BaseConfigParser):
+    def __init__(self, cfg_obj):
+        super().__init__(cfg_obj)
+        self.parser_helper = CoreParserHelper(cfg_obj)
+
+
     def get_metadata(self):
         return {'pci': self.handle_pci,
                 'memory': self.handle_memory,
@@ -211,29 +173,9 @@ class DevConfig(BaseConfigParser):
                 pci_obj.add_config(dev_attr['config'])
             self.cfg.CONFIG_PCI[vid_str][name] = pci_obj
 
-    def _make_reg_name(self, vid_str, device_name, reg_name):
-        return '.'.join([vid_str, device_name, reg_name])
-    
-    def _process_bar(self, vid_str, bar_name, bar_attr, dest, cfg_obj):
-        if 'register' in bar_attr:
-            bar_attr['register'] = self._make_reg_name(vid_str, bar_attr['device'], bar_attr['register'])
-        if 'base_reg' in bar_attr:
-            bar_attr['base_reg'] = self._make_reg_name(vid_str, bar_attr['device'], bar_attr['base_reg'])
-        if 'mmio_base' in bar_attr:
-            bar_attr['mmio_base'] = self._make_reg_name(vid_str, bar_attr['device'], bar_attr['mmio_base'])
-        if 'limit_register' in bar_attr:
-            bar_attr['limit_register'] = self._make_reg_name(vid_str, bar_attr['device'], bar_attr['limit_register'])
 
-        if vid_str not in dest:
-            dest[vid_str] = {}
-        if bar_attr['device'] not in dest[vid_str]:
-            dest[vid_str][bar_attr['device']] = {}
-        if bar_name in dest[vid_str][bar_attr['device']] and 'config' in bar_attr:
-            dest[vid_str][bar_attr['device']][bar_name].add_config(bar_attr['config'])
-        else:
-            bar_attr['ids'] = self.cfg.CONFIG_PCI[vid_str][bar_attr['device']].instances.values()
-            bar_obj = cfg_obj(bar_attr)
-            dest[vid_str][bar_attr['device']][bar_name] = bar_obj
+    
+
 
     def _process_def(self, dest, et_node, tag, stage_data, cfg_obj):
         ret_val = []
@@ -273,18 +215,7 @@ class DevConfig(BaseConfigParser):
 
         return ret_val
 
-    def _process_config_complex(self, stage_data, dev_name, dev_attr, component=None):
-        ret_val = []
-
-        attrs = {}
-        if dev_attr.config:
-            attrs['tmp'] = dev_attr.instances
-            for fxml in dev_attr.config:
-                cfg_file = fxml.replace('.', os.path.sep, fxml.count('.') - 1)
-                cfg_path = os.path.join(os.path.dirname(stage_data.xml_file), cfg_file)
-                ret_val.append(config_data(stage_data.vid_str, dev_name, cfg_path, component, attrs))
-
-        return ret_val
+    
 
     def handle_pci(self, et_node, stage_data):
         ret_val = []
@@ -294,7 +225,7 @@ class DevConfig(BaseConfigParser):
                 continue
             dev_name = dev_attr['name']
             self._process_pci_dev(stage_data.vid_str, dev_name, dev_attr)
-            ret_val.extend(self._process_config_complex(stage_data, dev_name, self.cfg.CONFIG_PCI[stage_data.vid_str][dev_name]))
+            ret_val.extend(self.parser_helper.process_config_complex(stage_data, dev_name, self.cfg.CONFIG_PCI[stage_data.vid_str][dev_name]))
             hex_dict = make_dict_hex(dev_attr)
             self.logger.log_debug(f"    + {dev_attr['name']:16}: {hex_dict}")
             for subcomponent in dev.iter('subcomponent'):
@@ -318,53 +249,38 @@ class DevConfig(BaseConfigParser):
     def handle_msr(self, et_node, stage_data): ## TODO
         return self._process_def(self.cfg.MSR, et_node, 'definition', stage_data, MSRConfig)
 
-    def _handle_bars(self, et_node, stage_data, dest, cfg_obj):
-        ret_val = []
-
-        for bar in et_node.iter('bar'):
-            ret_val.extend(self._handle_bar(bar, stage_data, dest, cfg_obj))
-            
-        return ret_val
-
-    def _handle_bar(self, et_node, stage_data, dest, cfg_obj):
-        ret_val = []
-        bus_attr = _config_convert_data(et_node, True)
-        if 'name' not in bus_attr or ('device' not in bus_attr and 'component' not in bus_attr):
-            self.logger.log_debug(f"Missing 'name' or 'device' in {bus_attr}")
-            return ret_val
-        bar_name = bus_attr['name']
-        dev_name = bus_attr['device'] = bus_attr['device'] if 'device' in bus_attr else bus_attr['component']
-        self._process_bar(stage_data.vid_str, bar_name, bus_attr, dest, cfg_obj)
-        # ret_val = (self._process_config(stage_data, dev_name, bus_attr))
-        ret_val.extend(self._process_config_complex(stage_data, bar_name, dest[stage_data.vid_str][dev_name][bar_name], dev_name))
-        hex_dict = make_dict_hex(bus_attr)
-        self.logger.log_debug(f"    + {bus_attr['name']:16}: {hex_dict}")
-        return ret_val
+    
 
     def handle_mmio_subcomponent(self, et_node, stage_data):
         dest = self.cfg.MMIO_BARS
-        return self._handle_bar(et_node, stage_data, dest, MMIOBarConfig)
+        return self.parser_helper.handle_bar(et_node, stage_data, dest, MMIOBarConfig)
         # return self._process_config_complex(stage_data, bar_name, dest[stage_data.vid_str][dev_name][bar_name], dev_name)
     
     def handle_io_subcomponent(self, et_node, stage_data):
         dest = self.cfg.IO_BARS
-        return self._handle_bar(et_node, stage_data, dest, IOBarConfig)
+        return self.parser_helper.handle_bar(et_node, stage_data, dest, IOBarConfig)
         # return self._process_config_complex(stage_data, bar_name, dest[stage_data.vid_str][dev_name][bar_name], dev_name)
 
     def handle_mmiobar(self, et_node, stage_data):
-        return self._handle_bars(et_node, stage_data, self.cfg.MMIO_BARS, MMIOBarConfig)
+        return self.parser_helper.handle_bars(et_node, stage_data, self.cfg.MMIO_BARS, MMIOBarConfig)
 
     def handle_iobar(self, et_node, stage_data):
-        return self._handle_bars(et_node, stage_data, self.cfg.IO_BARS, IOBarConfig)
+        return self.parser_helper.handle_bars(et_node, stage_data, self.cfg.IO_BARS, IOBarConfig)
 
 
 
 class CoreConfig(BaseConfigParser):
+    def __init__(self, cfg_obj):
+        super().__init__(cfg_obj)
+        self.parser_helper = CoreParserHelper(cfg_obj)
+
     def get_metadata(self):
         return {'ima': self.handle_ima,
                 'registers': self.handle_registers,
                 'controls': self.handle_controls,
-                'locks': self.handle_locks}
+                'locks': self.handle_locks,
+                'mmio': self.handle_mmio,
+                'io': self.handle_io}
 
     def get_stage(self):
         return Stage.CORE_SUPPORT
@@ -555,6 +471,12 @@ class CoreConfig(BaseConfigParser):
             else:
                 retval.append(None)
         return retval
+    
+    def handle_mmio(self, et_node, stage_data):
+        return self.parser_helper.handle_bars(et_node, stage_data, self.cfg.MMIO_BARS, MMIOBarConfig)
+
+    def handle_io(self, et_node, stage_data):
+        return self.parser_helper.handle_bars(et_node, stage_data, self.cfg.IO_BARS, IOBarConfig)
     
 
 
