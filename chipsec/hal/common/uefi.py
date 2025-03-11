1e60292539c9784dab7aaf56d41ffab4f7810131
Nathaniel Mitchell <nathaniel.p.mitchell@intel.com>
2025-03-10 17:19:07 -0700

    Fix acpi tests
    Signed-off-by: Nathaniel Mitchell <nathaniel.p.mitchell@intel.com>

--- chipsec/hal/common/uefi.py
+++ chipsec/hal/common/uefi.py
@@ -274,6 +274,8 @@ class UEFI(hal_base.HALBase):
                 logger().log_hal(f'[uefi] Unable to read memory at pa: {pa:016X} Error: {err}')
                 pa -= CHUNK_SZ
                 continue
+            if membuf is None:
+                breakpoint()
             pos = bytestostring(membuf).find(table_sig)
             if -1 != pos:
                 table_pa = pa + pos
