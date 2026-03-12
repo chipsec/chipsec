4928b27345631a808b5b21474747cd73c2f9f7d5
Nathaniel Mitchell <nathaniel.p.mitchell@intel.com>
2026-03-12 13:23:09 -0700

    Refactor platform>ip/bar classes
    Signed-off-by: Nathaniel Mitchell <nathaniel.p.mitchell@intel.com>

--- chipsec/cfg/parsers/ip/platform.py
+++ chipsec/cfg/parsers/ip/platform.py
@@ -41,6 +41,10 @@ class Recursable:
     This class provides the foundation for hierarchical traversal of platform configurations,
     supporting wildcard matching and recursive object discovery.
     """
+    
+    def __init__(self) -> None:
+        """Initialize Recursable object."""
+        super().__init__()
 
     def _get_next_level_list(self) -> List[str]:
         """
@@ -99,6 +103,20 @@ class Recursable:
         """
         raise NotImplementedError("_get_next_level() not implemented")
 
+    def get_register_matches_recursive(self, register_name: str) -> ObjList:
+        """Recursively collect register matches from this object and all descendants."""
+        registers = ObjList()
+
+        # Include direct register matches for register-bearing nodes.
+        if isinstance(self, RegisterList):
+            registers.extend(RegisterList.get_register_matches(self, register_name))
+
+        # Recurse through all child objects in the hierarchy.
+        for child in self.get_next_levels('*'):
+            registers.extend(child.get_register_matches_recursive(register_name))
+
+        return registers
+
 
 class RegisterList:
     """
@@ -110,6 +128,7 @@ class RegisterList:
 
     def __init__(self) -> None:
         """Initialize empty register list."""
+        super().__init__()
         self.register_list = {}
 
     def add_register(self, register_name: str, register_object: Any) -> None:
@@ -234,19 +253,15 @@ class Platform(Recursable):
         self.__delattr__(f'_{vendor.name}')
 
     def get_register_matches(self, register_name: str) -> ObjList:
-        """Search all vendors for registers matching the given name pattern.
+        """Search this platform hierarchy for registers matching the given name pattern.
 
         Args:
             register_name: Register name pattern (supports wildcards)
 
         Returns:
-            ObjList containing matching registers from all vendors
+            ObjList containing matching registers from all descendants
         """
-        registers = ObjList()
-        for vendor_name in self.vendor_list:
-            vendor = self.get_vendor(vendor_name)
-            registers.extend(vendor.get_register_matches(register_name))
-        return registers
+        return self.get_register_matches_recursive(register_name)
 
     def _get_next_level_list(self) -> List[str]:
         """Get list of vendor names."""
@@ -530,22 +545,15 @@ class Vendor(Recursable):
             raise PlatformConfigError(f'Device: {ip_name} not found in Vendor: {self.name}')
 
     def get_register_matches(self, register_name: str) -> ObjList:
-        """Search all IPs and BARs for registers matching the given name pattern.
+        """Search this vendor hierarchy for registers matching the given name pattern.
 
         Args:
             register_name: Register name pattern (supports wildcards)
 
         Returns:
-            ObjList containing matching registers from all IPs and BARs
+            ObjList containing matching registers from all descendants
         """
-        registers = ObjList()
-        for ip_name in self.ip_list:
-            ip = self.get_ip(ip_name)
-            registers.extend(ip.get_register_matches(register_name))
-            for bar_name in ip.bar_list:
-                bar = ip.get_bar(bar_name)
-                registers.extend(bar.get_register_matches(register_name))
-        return registers
+        return self.get_register_matches_recursive(register_name)
 
     def _get_next_level_list(self) -> List[str]:
         """Get list of IP names."""
@@ -556,40 +564,20 @@ class Vendor(Recursable):
         return self.get_ip(ip_name)
 
 
-class IP(Recursable, RegisterList):
-    """
-    IP (Intellectual Property) configuration container.
+class BarContainerMixin(Recursable):
+    """Shared BAR container behavior used by both IP and BAR objects."""
 
-    This class manages BAR configurations and registers for a specific IP block,
-    providing hierarchical access to IP-specific components.
-    """
-
-    def __init__(self, name: str, ipobj: Any) -> None:
-        """
-        Initialize IP configuration.
-
-        Args:
-            name: Name of the IP
-            ipobj: IP configuration object
-        """
+    _container_type = 'Unknown'
+    
+    def __init__(self) -> None:
+        """Initialize BAR container."""
         super().__init__()
-        RegisterList.__init__(self)
         self.bar_list = []
-        self.name = name
-        self.obj = ipobj
-
-    def update_obj(self, ipobj: Any) -> None:
-        """
-        Update the object of the IP.
-
-        Args:
-            ipobj: IP configuration object
-        """
-        self.obj = ipobj
 
     def add_bar(self, bar_name: str, barobj: Any) -> None:
         """
-        Add a BAR configuration to the IP.
+            bar_name: Name of the BAR
+        Add a BAR configuration to the current object.
 
         Args:
             bar_name: Name of the BAR
@@ -604,7 +592,6 @@ class IP(Recursable, RegisterList):
         Get a BAR configuration by name.
 
         Args:
-            bar_name: Name of the BAR
 
         Returns:
             BAR configuration object
@@ -638,16 +625,46 @@ class IP(Recursable, RegisterList):
         if bar_id in self._get_next_level_list():
             return self.get_bar(bar_id)
         else:
-            raise PlatformConfigError(f'Next Level: {bar_id} not found in IP: {self.name}')
+            raise PlatformConfigError(f'Next Level: {bar_id} not found in {self._container_type}: {self.name}')
 
 
-class Bar(Recursable, RegisterList):
+class IP(BarContainerMixin, RegisterList):
+    """
+    IP (Intellectual Property) configuration container.
+
+    This class manages BAR configurations and registers for a specific IP block,
+    providing hierarchical access to IP-specific components.
+    """
+    _container_type = 'IP'
+    def __init__(self, name: str, ipobj: Any) -> None:
+        """
+        Initialize IP configuration.
+
+        Args:
+            name: Name of the IP
+            ipobj: IP configuration object
+        """
+        super().__init__()
+        self.name = name
+        self.obj = ipobj
+
+    def update_obj(self, ipobj: Any) -> None:
+        """
+        Update the object of the IP.
+
+        Args:
+            ipobj: IP configuration object
+        """
+        self.obj = ipobj
+
+class Bar(BarContainerMixin, RegisterList):
     """
     BAR (Base Address Register) configuration container.
 
     This class manages registers within a specific BAR and provides
     hierarchical access to BAR-specific registers.
     """
+    _container_type = 'BAR'
 
     def __init__(self, name: str, barobj: Any) -> None:
         """
@@ -658,28 +675,6 @@ class Bar(Recursable, RegisterList):
             barobj: BAR configuration object
         """
         super().__init__()
-        RegisterList.__init__(self)
         self.name = name
         self.obj = barobj
 
-    def _get_next_level_list(self) -> List[str]:
-        """Get list of register names."""
-        return list(self.register_list.keys())
-
-    def _get_next_level(self, register_id: str) -> ObjList:
-        """
-        Get next level object (register) by ID.
-
-        Args:
-            register_id: Register identifier
-
-        Returns:
-            Register object list
-
-        Raises:
-            PlatformConfigError: If register not found
-        """
-        if register_id in self.register_list.keys():
-            return self.get_register(register_id)
-        else:
-            raise PlatformConfigError(f'Register: {register_id} not found in BAR: {self.name}')
