79fbc5aa69d7082219d072961894c1bd5a8c8193
Nathaniel Mitchell <nathaniel.p.mitchell@intel.com>
2026-07-15 12:34:27 -0700

    Update chipsec's banner printing functions and print pch when detected

--- chipsec/library/display.py
+++ chipsec/library/display.py
@@ -25,16 +25,18 @@ Display functions
 import platform
 import sys
 from datetime import datetime
-from typing import Tuple, Sequence
+from typing import Sequence
 
 from chipsec.chipset import Chipset
+from chipsec.library.defines import os_version, get_version, get_message
 from chipsec.library.gil import gil_status
 from chipsec.library.logger import logger
 
 
-def chipsec_banner(arguments: Sequence[str], version: str, message: str, custom_str: str = '') -> str:
+def _chipsec_banner(arguments: Sequence[str], custom_str: str = '') -> str:
     """Creates the CHIPSEC banner string"""
     args = ' '.join(arguments)
+    message = get_message()
     start_time = datetime.now().isoformat().replace('T',' ').split('.')[0]
     if custom_str:
         message += f'\n{custom_str}'
@@ -44,42 +46,29 @@ def chipsec_banner(arguments: Sequence[str], version: str, message: str, custom_
 ##  CHIPSEC: Platform Hardware Security Assessment Framework  ##
 ##                                                            ##
 ################################################################
-[CHIPSEC] Version  : {version}
+[CHIPSEC] Version  : {get_version()}
 [CHIPSEC] Start    : {start_time}
 [CHIPSEC] Arguments: {args}
 {message}'''
     return banner
 
 
-def print_banner(arguments: Sequence[str], version: str, message: str, custom_str: str = '') -> None:
-    logger().log(chipsec_banner(arguments, version, message, custom_str))
+def print_banner(arguments: Sequence[str], custom_str: str = '') -> None:
+    logger().log(_chipsec_banner(arguments, custom_str))
 
 
-def chipsec_banner_properties(cs: Chipset, os_version: Tuple[str, str, str, str]) -> str:
+def _chipsec_environment(cs: Chipset) -> str:
     """Creates the CHIPSEC properties banner string"""
-    (system, release, version, machine) = os_version
+    (system, release, version, machine) = os_version()
     is_python_64 = True if (sys.maxsize > 2**32) else False
     python_version = platform.python_version()
     python_arch = '64-bit' if is_python_64 else '32-bit'
     (helper_name, driver_path) = cs.helper.get_info()
-    include_pch_str = cs.Cfg.is_pch_req() or (cs.Cfg.is_pch_req() is None)
 
     banner_prop = f'''
 [CHIPSEC] OS      : {system} {release} {version} {machine}
 [CHIPSEC] Python  : {python_version} ({python_arch}) - {gil_status()} GIL
 [CHIPSEC] Helper  : {helper_name} {driver_path}
-[CHIPSEC] Mfg ID  : {cs.Cfg.mfgid}
-[CHIPSEC] Platform: {cs.Cfg.longname}
-[CHIPSEC]    CPUID: {cs.Cfg.cpuid:05X}
-[CHIPSEC]      VID: {cs.Cfg.vid:04X}
-[CHIPSEC]      DID: {cs.Cfg.did:04X}
-[CHIPSEC]      RID: {cs.Cfg.rid:02X}'''
-    if include_pch_str:
-        banner_prop += f'''
-[CHIPSEC] PCH     : {cs.Cfg.pch_longname}
-[CHIPSEC]      VID: {cs.Cfg.pch_vid:04X}
-[CHIPSEC]      DID: {cs.Cfg.pch_did:04X}
-[CHIPSEC]      RID: {cs.Cfg.pch_rid:02X}
 '''
     if not is_python_64 and machine.endswith('64'):
         banner_prop += 'Python architecture (32-bit) is different from OS architecture (64-bit)'
@@ -87,11 +76,17 @@ def chipsec_banner_properties(cs: Chipset, os_version: Tuple[str, str, str, str]
     return banner_prop
 
 
-def print_banner_properties(cs: Chipset, os_version: Tuple[str, str, str, str]) -> None:
-
+def print_environment(cs: Chipset) -> None:
     if not cs.load_config:
         logger().log_warning('Not loading configurations. Platform will remain unknown.')
-    logger().log(chipsec_banner_properties(cs, os_version))
+    logger().log(_chipsec_environment(cs))
+
+
+def print_chipsec_info(cs: Chipset) -> None:
+    print_environment(cs)
+    cs.Cfg.print_platform_info()
+    if cs.Cfg.is_pch_req() or cs.Cfg.is_pch_detected():
+        cs.Cfg.print_pch_info()
 
 
 def make_dict_hex(int_dict: dict) -> dict:
